b521b68b3ef93dea90b09bcc40656d2e91918f6b
itizawa <itizawa@weseek.co.jp>
2021-01-01 13:14:37 +0900

    fix document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
 import theme from '../src/theme';
-export class MyDocument extends Document {
+export default class MyDocument extends Document {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   render() {
     return (
