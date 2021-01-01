2d62383e0b2d65a13bf26c51b9ed6b93d879968c
itizawa <itizawa@weseek.co.jp>
2021-01-01 12:51:29 +0900

    setup next

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h2" gutterBottom>
+          Next.js example
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
