548d359a5c314bb224d078da85d339e47e537a76
itizawa <itizawa@weseek.co.jp>
2021-01-02 20:26:02 +0900

    use css module

--- src/components/organisms/SidebarList/SidebarList.tsx
+++ src/components/organisms/SidebarList/SidebarList.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react';
 import ClassNames from 'classnames';
+import './SidebarList.module.css';
 import { HomeIcon } from '~src/components/icons/HomeIcon';
 
 type Props = {
@@ -12,7 +13,7 @@ const SidebarListItem: FC<Props> = (props: Props) => {
   const { isActive, isDisabled, children } = props;
 
   const classNameForListItem = ClassNames({
-    'list-group-item list-group-item-action': true,
+    'list-group-item list-group-item-action mx-3': true,
     active: isActive,
     disabled: isDisabled,
   });
@@ -22,10 +23,10 @@ const SidebarListItem: FC<Props> = (props: Props) => {
 
 export const SidebarList: FC = () => {
   return (
-    <ul className="list-group list-group-flush">
+    <ul className="sidebar-list-group list-group list-group-flush">
       <SidebarListItem isActive>
         <HomeIcon />
-        Home
+        <span className="ms-2">Home</span>
       </SidebarListItem>
       <SidebarListItem>test</SidebarListItem>
       <SidebarListItem isDisabled>disabled</SidebarListItem>
