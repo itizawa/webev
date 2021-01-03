c0e6e9c15a8981b40a3da392aa2552b3e1a5481a
itizawa <itizawa@weseek.co.jp>
2021-01-03 23:37:19 +0900

    refactor dir

--- src/stories/organisms/SidebarList.stories.tsx
+++ src/stories/organisms/SidebarList.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentProps, useState } from 'react';
 import { Story } from '@storybook/react/types-6-0';
 
-import { SidebarList } from '.';
+import { SidebarList } from '~src/components/organisms/SidebarList';
 
 export default {
   title: 'organisms/SidebarList',
