f2bef77b2df53a8f83771bd24fe8cf71c6f4534c
itizawa <itizawa@weseek.co.jp>
2021-01-09 23:37:57 +0900

    add type

--- src/pages/api/ogp.ts
+++ src/pages/api/ogp.ts
@@ -1,17 +1,18 @@
 import nextConnect from 'next-connect';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ogp = require('ogp-parser');
 
 const handler = nextConnect();
 
-handler.get(async (req: any, res: any) => {
+handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
   const { url } = req.query;
 
   try {
-    const ogpData = await ogp(url);
+    const res = await ogp(url);
 
-    return res.status(200).send({ ogpData });
+    return res.status(200).send(res);
   } catch (err) {
     return res.status(500).send({ success: false });
   }
