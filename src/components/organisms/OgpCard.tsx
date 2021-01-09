04c7b4a3c949a6f5742752aa35a32bc9397fac84
itizawa <itizawa@weseek.co.jp>
2021-01-10 00:21:28 +0900

    add cheerio

--- src/components/organisms/OgpCard.tsx
+++ src/components/organisms/OgpCard.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import axios from 'axios';
 
 type Props = {
@@ -7,14 +7,27 @@ type Props = {
 
 export const OgpCard: FC<Props> = (props: Props) => {
   const { url } = props;
+  const [image, setImage] = useState();
+  const [description, setDescription] = useState();
 
   useEffect(() => {
     const retrieveOgp = async (): Promise<void> => {
       const res = await axios.get(`/api/ogp?url=${url}`);
+      const { image, desc } = res.data;
       console.log(res.data);
+
+      setImage(image);
+      setDescription(desc);
     };
     retrieveOgp();
   }, [url]);
 
-  return <p>{url}</p>;
+  return (
+    <div className="card">
+      <img src={image} alt={image} />
+      <div className="card-body">
+        <p className="card-text">{description}</p>
+      </div>
+    </div>
+  );
 };
--- src/pages/api/ogp.ts
+++ src/pages/api/ogp.ts
@@ -1,18 +1,30 @@
 import nextConnect from 'next-connect';
 import type { NextApiRequest, NextApiResponse } from 'next';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ogp = require('ogp-parser');
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 const handler = nextConnect();
+
+type ogpResponse = {
+  image?: string;
+  desc?: string;
+  title?: string;
+};
 
 handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
   const { url } = req.query;
 
   try {
-    const result = await ogp(url);
+    const result = await axios.get(url as string);
+    const $ = cheerio.load(result.data);
 
-    return res.status(200).send(result);
+    const response: ogpResponse = {
+      image: $("meta[property='og:image']").attr('content'),
+      desc: $("meta[property='og:description']").attr('content'),
+      title: $("meta[property='og:title']").attr('content'),
+    };
+
+    return res.status(200).send(response);
   } catch (err) {
     return res.status(500).send({ success: false });
   }
