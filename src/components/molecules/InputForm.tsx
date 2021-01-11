cd3b13af7c1b783ff3d71faf192c88d20ec94d6b
itizawa <itizawa@weseek.co.jp>
2021-01-11 13:52:10 +0900

    create component

---
+++ src/components/molecules/InputForm.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+
+type Props = {
+  inputValue: string;
+  onChangeInputValue: (value: string) => void;
+  onClickSaveBtn: () => void;
+};
+
+export const InputForm: FC<Props> = (props: Props) => {
+  const { inputValue } = props;
+
+  const handleChangeValue = (inputValue: string) => {
+    if (props.onChangeInputValue != null) {
+      props.onChangeInputValue(inputValue);
+    }
+  };
+
+  const handleClickSaveBtn = () => {
+    if (props.onClickSaveBtn != null) {
+      props.onClickSaveBtn();
+    }
+  };
+
+  return (
+    <div className="input-group mb-3">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="URL を保存"
+        aria-label="Input Group"
+        aria-describedby="input-group"
+        value={inputValue}
+        onChange={(e) => handleChangeValue(e.target.value)}
+      />
+      <button className="btn btn-outline-secondary" type="button" id="input-group" onClick={handleClickSaveBtn}>
+        保存する
+      </button>
+    </div>
+  );
+};
