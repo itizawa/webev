c1e9e8f08f8e900f61ef080bc75453e771efff53
itizawa <itizawa@weseek.co.jp>
2021-02-10 23:32:39 +0900

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  },
+};
