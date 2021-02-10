bb685ec4bf4cbe906e7515d8686d9d526110d9ce
itizawa <itizawa@weseek.co.jp>
2021-02-10 23:39:05 +0900

    MONGO_URI: add

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ module.exports = {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    MONGO_URI: process.env.MONGO_URI,
   },
 };
