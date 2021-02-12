11bb6cc0f9836dd65cd1565a2590ab3bcf7b2d9c
itizawa <itizawa@weseek.co.jp>
2021-02-13 01:34:46 +0900

    use RestClient

--- src/stores/page.tsx
+++ src/stores/page.tsx
@@ -1,10 +1,10 @@
 import useSWR, { responseInterface } from 'swr';
-import { apiGet } from '~/utils/rest-client';
+import { restClient } from '~/utils/rest-client';
 
 import { IPage } from '~/interfaces/page';
 
 export const usePageListSWR = (): responseInterface<IPage[], Error> => {
-  return useSWR('/pages/list', (endpoint) => apiGet(endpoint).then((result) => result.data), {
+  return useSWR('/pages/list', (endpoint) => restClient.apiGet(endpoint).then((result) => result.data), {
     revalidateOnFocus: false,
     revalidateOnReconnect: true,
   });
