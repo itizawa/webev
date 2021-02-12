eb86f8d3b05125fb07cd28529602e4f4e5fb5c69
itizawa <itizawa@weseek.co.jp>
2021-02-13 01:34:40 +0900

    create RestClient class

--- src/utils/rest-client.ts
+++ src/utils/rest-client.ts
@@ -1,20 +1,39 @@
-import axiosBase from 'axios';
+import axiosBase, { AxiosInstance } from 'axios';
+import { getSession } from 'next-auth/client';
 
-const axios = axiosBase.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000',
-  headers: {
-    'Content-Type': 'application/json',
-    'X-Requested-With': 'XMLHttpRequest',
-  },
-  responseType: 'json',
-});
+class RestClient {
+  axios: AxiosInstance;
+  accessToken?: string;
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const apiGet = (url: string, query = {}): Promise<any> => {
-  return axios.get(`/api/v1${url}`, query);
-};
+  constructor() {
+    this.axios = axiosBase.create({
+      baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+      responseType: 'json',
+    });
+  }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const apiPost = (url: string, body = {}): Promise<any> => {
-  return axios.post(`/api/v1${url}`, body);
-};
+  async getAccessToken() {
+    if (this.accessToken == null) {
+      const session = await getSession();
+      this.accessToken = session?.accessToken;
+    }
+    return this.accessToken;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async apiGet(url: string, query = {}): Promise<any> {
+    const accessToken = await this.getAccessToken();
+    return this.axios.get(`/api/v1${url}`, { ...query, headers: { Authorization: `Bearer ${accessToken}` } });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  apiPost(url: string, body = {}): Promise<any> {
+    return this.axios.post(`/api/v1${url}`, body);
+  }
+}
+
+export const restClient = new RestClient();
