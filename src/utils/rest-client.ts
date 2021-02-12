cc0e79186ed7c009940fc7c336a2b8564829e2ec
itizawa <itizawa@weseek.co.jp>
2021-02-13 01:59:50 +0900

    update type AxiosResponse

--- src/utils/rest-client.ts
+++ src/utils/rest-client.ts
@@ -1,4 +1,4 @@
-import axiosBase, { AxiosInstance } from 'axios';
+import axiosBase, { AxiosInstance, AxiosResponse } from 'axios';
 import { getSession } from 'next-auth/client';
 
 class RestClient {
@@ -24,14 +24,12 @@ class RestClient {
     return this.accessToken;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  async apiGet(url: string, query = {}): Promise<any> {
+  async apiGet(url: string, query = {}): Promise<AxiosResponse> {
     const accessToken = await this.getAccessToken();
     return this.axios.get(`/api/v1${url}`, { ...query, headers: { Authorization: `Bearer ${accessToken}` } });
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  async apiPost(url: string, body = {}): Promise<any> {
+  async apiPost(url: string, body = {}): Promise<AxiosResponse> {
     const accessToken = await this.getAccessToken();
     return this.axios.post(`/api/v1${url}`, body, { headers: { Authorization: `Bearer ${accessToken}` } });
   }
