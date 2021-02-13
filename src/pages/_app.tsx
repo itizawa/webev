39a1a2c8b791805731478f0bf9d0cfd84468f49b
itizawa <itizawa@weseek.co.jp>
2021-02-13 18:55:22 +0900

    class to main

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,14 +14,14 @@ const App = ({ Component, pageProps }: AppProps) => (
       <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
     </Head>
     <Navbar />
-    <div className="d-flex h-100 margin-wide-display mx-auto pt-lg-4">
+    <main className="d-flex h-100 mx-auto pt-lg-4">
       <div className="d-none d-lg-block col-lg-2">
         <Sidebar />
       </div>
       <div className="col-12 col-lg-10">
         <Component {...pageProps} />
       </div>
-    </div>
+    </main>
   </>
 );
 
