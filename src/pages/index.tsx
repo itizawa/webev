099ad75860bce28cd505636f5c8e225955b8d73e
itizawa <itizawa@weseek.co.jp>
2021-02-18 01:06:32 +0900

    implment index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,13 @@
 import { FC } from 'react';
-import Skeleton from 'react-loading-skeleton';
-
-import { usePageListSWR } from '~/stores/page';
-import { OgpCard } from '~/components/organisms/OgpCard';
+import Link from 'next/link';
 
 const Index: FC = () => {
-  const { data: pages } = usePageListSWR();
-
   return (
     <div className="p-3">
-      <h1>Home</h1>
-      <div className="row mt-4">
-        {pages == null ? (
-          <>
-            <div className="col-lg-4 col-md-6">
-              <Skeleton height={300} />
-            </div>
-            <div className="col-lg-4 col-md-6">
-              <Skeleton height={300} />
-            </div>
-            <div className="col-lg-4 col-md-6">
-              <Skeleton height={300} />
-            </div>
-          </>
-        ) : (
-          pages.map((page) => (
-            <div className="col-lg-4 col-md-6 mb-3" key={page._id}>
-              <OgpCard page={page} />
-            </div>
-          ))
-        )}
-      </div>
+      <h1>Welcome Webev!!</h1>
+      <h2>
+        <Link href="/home">Go to Home</Link>
+      </h2>
     </div>
   );
 };
