db924ae31448cca10810557db477d20dfec6ec0c
itizawa <itizawa@weseek.co.jp>
2021-02-20 23:01:38 +0900

    create static swr

--- src/stores/use-static-swr.tsx
+++ src/stores/use-static-swr.tsx
@@ -1,6 +1,11 @@
 import useSWR, { keyInterface, responseInterface, mutate, cache } from 'swr';
+import { fetcherFn } from 'swr/dist/types';
 
-export const useStaticSWR = <Data, Error>(key: keyInterface, updateData?: Data, initialData?: Data): responseInterface<Data, Error> => {
+export const useStaticSWR = <Data, Error>(
+  key: keyInterface,
+  updateData?: Data | fetcherFn<Data>,
+  initialData?: Data | fetcherFn<Data>,
+): responseInterface<Data, Error> => {
   if (updateData == null) {
     if (!cache.has(key) && initialData != null) {
       mutate(key, initialData, false);
@@ -9,7 +14,7 @@ export const useStaticSWR = <Data, Error>(key: keyInterface, updateData?: Data, 
     mutate(key, updateData);
   }
 
-  return useSWR(key, {
+  return useSWR(key, null, {
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
   });
