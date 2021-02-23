cc7989fec186b436e7a8d58e085205a6bc0bfb24
itizawa <itizawa@weseek.co.jp>
2021-02-23 16:57:34 +0900

    impl useOffsetPage

--- src/stores/page.tsx
+++ src/stores/page.tsx
@@ -2,16 +2,23 @@ import useSWR, { responseInterface } from 'swr';
 import { restClient } from '~/utils/rest-client';
 
 import { Page } from '~/interfaces/page';
+import { useStaticSWR } from '~/stores/use-static-swr';
+
+export const useOffsetPage = (initialData?: number): responseInterface<number | null, Error> => {
+  return useStaticSWR('offsetPage', initialData);
+};
 
 export const usePageListSWR = (): responseInterface<Page[], Error> => {
-  return useSWR('/pages/list', (endpoint) => restClient.apiGet(endpoint).then((result) => result.data), {
+  const { data: offsetPage } = useOffsetPage();
+  return useSWR(`/pages/list?offset=${offsetPage || 0}`, (endpoint) => restClient.apiGet(endpoint).then((result) => result.data), {
     revalidateOnFocus: false,
     revalidateOnReconnect: true,
   });
 };
 
 export const useFavoritePageListSWR = (): responseInterface<Page[], Error> => {
-  return useSWR('/pages/favorite-list', (endpoint) => restClient.apiGet(endpoint).then((result) => result.data), {
+  const { data: offsetPage } = useOffsetPage();
+  return useSWR(`/pages/favorite-list?limit=${offsetPage || 0}`, (endpoint) => restClient.apiGet(endpoint).then((result) => result.data), {
     revalidateOnFocus: false,
     revalidateOnReconnect: true,
   });
