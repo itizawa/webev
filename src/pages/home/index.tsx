d229bdc5679839edb885576975c99d33404d8da9
itizawa <itizawa@weseek.co.jp>
2021-02-26 00:43:57 +0900

    implement scroll

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,23 +1,34 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import InfiniteScroll from 'react-infinite-scroller';
 
 import { usePageListSWR } from '~/stores/page';
 import { OgpCard } from '~/components/organisms/OgpCard';
 
-const Index: FC = () => {
-  const [cnt, setCnt] = useState(1);
-  const pages = [];
-  for (let i = 0; i < cnt; i++) {
-    pages.push(<Page activePage={i + 1} key={i} />);
-  }
+import { Page } from '~/interfaces/page';
 
-  const loadMore = async (page: number) => {
-    setCnt(page + 1);
+const Index: FC = () => {
+  const [activePage, setCnt] = useState(1);
+  const [hasPrevPage, setHasPrevPage] = useState(false);
+  const [pages, setPages] = useState([] as Page[]);
+  const { data: paginationResult, isValidating } = usePageListSWR(activePage);
+
+  const loadMore = async (activePage: number) => {
+    if (!isValidating) {
+      setHasPrevPage(false);
+      setCnt(activePage + 1);
+    }
   };
 
+  useEffect(() => {
+    if (paginationResult != null) {
+      setPages([...pages, ...paginationResult.docs]);
+      setHasPrevPage(paginationResult.hasNextPage);
+    }
+  }, [paginationResult]);
+
   const skeletonForLoading = (
-    <div className="row mt-4">
+    <div className="row mt-4" key={0}>
       {[...Array(9)].map((_, i) => (
         <div key={i} className="col-lg-4 col-md-6 mb-3">
           <Skeleton height={300} />
@@ -29,42 +40,15 @@ const Index: FC = () => {
   return (
     <div className="p-3">
       <h1>Home</h1>
-      <InfiniteScroll loadMore={loadMore} hasMore={true} loader={skeletonForLoading} element="div">
-        {pages}
-      </InfiniteScroll>
-    </div>
-  );
-};
-
-type Props = {
-  activePage: number;
-};
-
-const Page: FC<Props> = ({ activePage }: Props) => {
-  const { data: paginationResult } = usePageListSWR(activePage);
-
-  // 取得中の場合は スケルトンを表示する
-  if (paginationResult == null) {
-    return (
-      <div className="row mt-4">
-        {[...Array(9)].map((_, i) => (
-          <div key={i} className="col-lg-4 col-md-6 mb-3">
-            <Skeleton height={300} />
-          </div>
-        ))}
-      </div>
-    );
-  }
-
-  const { docs: pages } = paginationResult;
-
-  return (
-    <div className="row mt-4">
-      {pages.map((page) => (
-        <div className="col-lg-4 col-md-6 mb-3" key={page._id}>
-          <OgpCard page={page} />
+      <InfiniteScroll loadMore={loadMore} hasMore={hasPrevPage} loader={skeletonForLoading} element="div">
+        <div className="row mt-4">
+          {pages.map((page) => (
+            <div className="col-lg-4 col-md-6 mb-3" key={page._id}>
+              <OgpCard page={page} />
+            </div>
+          ))}
         </div>
-      ))}
+      </InfiniteScroll>
     </div>
   );
 };
