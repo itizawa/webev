ac29e76ce00fb41b8e7df770d84ada7dacc087d1
itizawa <itizawa@weseek.co.jp>
2021-02-27 20:11:19 +0900

    impl useSWRInfinite

--- src/stores/page.tsx
+++ src/stores/page.tsx
@@ -1,12 +1,13 @@
-import useSWR, { responseInterface } from 'swr';
+import { useSWRInfinite, SWRInfiniteResponseInterface } from 'swr';
 import { restClient } from '~/utils/rest-client';
 
 import { PaginationResult } from '~/interfaces/paginationResult';
+
 import { Page } from '~/interfaces/page';
 
-export const usePageListSWR = (activePage = 1, limit = 9, isFavorite = false): responseInterface<PaginationResult<Page>, Error> => {
-  return useSWR(
-    `/pages/list?status=stocked&page=${activePage}&limit=${limit}&isFavorite=${isFavorite}`,
+export const usePageListSWR = (limit = 9, isFavorite = false): SWRInfiniteResponseInterface<PaginationResult<Page>, Error> => {
+  return useSWRInfinite(
+    (index) => `/pages/list?status=stocked&page=${index + 1}&limit=${limit}&isFavorite=${isFavorite}`,
     (endpoint) => restClient.apiGet(endpoint).then((result) => result.data),
     {
       revalidateOnFocus: false,
