c1869ddc814aaf4d64f62a476c3179b8f5a57957
itizawa <itizawa@weseek.co.jp>
2021-02-27 21:22:06 +0900

    adjust key

--- src/stores/page.tsx
+++ src/stores/page.tsx
@@ -5,12 +5,12 @@ import { PaginationResult } from '~/interfaces/paginationResult';
 import { Page } from '~/interfaces/page';
 
 export const usePageListSWR = (activePage = 1, limit = 9, isFavorite = false): responseInterface<PaginationResult<Page>, Error> => {
-  return useSWR(
-    `/pages/list?status=stocked&page=${activePage}&limit=${limit}&isFavorite=${isFavorite}`,
-    (endpoint) => restClient.apiGet(endpoint).then((result) => result.data),
-    {
-      revalidateOnFocus: false,
-      revalidateOnReconnect: true,
-    },
-  );
+  let key = `/pages/list?status=stocked&page=${activePage}&limit=${limit}`;
+  if (isFavorite) {
+    key += `&isFavorite=${isFavorite}`;
+  }
+  return useSWR(key, (endpoint) => restClient.apiGet(endpoint).then((result) => result.data), {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: true,
+  });
 };
