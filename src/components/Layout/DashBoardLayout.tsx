a2ce464c31e94f88038b2ad2c2bd38ed8c4564da
itizawa <itizawa@weseek.co.jp>
2021-02-28 16:18:57 +0900

    refacotr _app

--- src/components/Layout/DashBoardLayout.tsx
+++ src/components/Layout/DashBoardLayout.tsx
@@ -1,4 +1,8 @@
+import Head from 'next/head';
 import { FC } from 'react';
+import { useSession } from 'next-auth/client';
+
+import { SocketConnector } from '../SocketConnector';
 import style from '~/styles/navbarBorder.module.scss';
 
 import { Navbar } from '~/components/organisms/Navbar';
@@ -7,19 +11,26 @@ import { SubnavBar } from '~/components/organisms/SubnavBar';
 import { PageModals } from '~/components/PageModals/PageModals';
 
 export const DashBoardLayout: FC = ({ children }) => {
+  const [session] = useSession();
+
   return (
     <>
+      <Head>
+        <title>Webev</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
       <Navbar />
       <div className={`webev-nav-border ${style['nav-border']}`} />
-      <nav className="sticky-top bg-dark d-flex justify-content-evenly d-md-none">
+      <div className="sticky-top bg-dark d-flex justify-content-evenly d-md-none">
         <SubnavBar />
-      </nav>
+      </div>
       <main className="d-flex mx-auto pt-lg-4">
         <div className="d-none d-md-block col-lg-2">
           <Sidebar />
         </div>
         <div className="col-12 col-md-10">{children}</div>
-        <PageModals />
+        {session && <PageModals />}
+        {session && <SocketConnector />}
       </main>
     </>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,41 +1,12 @@
-import Head from 'next/head';
-import { useState, useEffect } from 'react';
-import io from 'socket.io-client';
 import { AppProps } from 'next/app';
 import { Provider } from 'next-auth/client';
 
 import '~/styles/global.scss';
 
-import { usePageListSWR } from '~/stores/page';
-
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const App = ({ Component, pageProps }: AppProps) => {
-  const [socket] = useState(() => io(process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000'));
-  const { mutate: pageListMutate } = usePageListSWR();
-
-  useEffect(() => {
-    socket.on('connect', () => {
-      console.log('socket connected!!');
-    });
-    socket.on('disconnect', () => {
-      console.log('socket disconnected!!');
-    });
-    socket.on('update-page', () => {
-      console.log('Get Updated Data');
-      pageListMutate();
-    });
-
-    return () => {
-      socket.close();
-    };
-  }, []);
-
   return (
     <Provider options={{ clientMaxAge: 0, keepAlive: 0 }} session={pageProps.session}>
-      <Head>
-        <title>Webev</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
       <Component {...pageProps} />
     </Provider>
   );
