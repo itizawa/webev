c6a9c0986ed38ecd2904c86bf181d5cefd6547dd
itizawa <itizawa@weseek.co.jp>
2021-02-28 15:58:06 +0900

    adjust LoginRequiredWrapper

--- src/components/Authentication/LoginRequiredWrapper.tsx
+++ src/components/Authentication/LoginRequiredWrapper.tsx
@@ -1,15 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Router } from 'next/router';
-import { useEffect } from 'react';
 import { signIn, useSession } from 'next-auth/client';
+import { FC } from 'react';
 import { DashBoardLayout } from '~/components/Layout/DashBoardLayout';
 
-export const LoginRequiredWrapper: any = (props: any) => {
+export const LoginRequiredWrapper: FC = ({ children }) => {
   const [session, loading] = useSession();
 
-  useEffect(() => {
-    console.log(session, loading);
-  }, [Router]);
   // When rendering client side don't display anything until loading is complete
   if (typeof window !== 'undefined' && loading) return null;
 
@@ -27,5 +22,5 @@ export const LoginRequiredWrapper: any = (props: any) => {
     );
   }
 
-  return <>{props.children}</>;
+  return <>{children}</>;
 };
