8ee9fb646ec2af6ff2f5764246c47393b59156ee
itizawa <itizawa@weseek.co.jp>
2021-02-28 16:18:10 +0900

    refactor socket

---
+++ src/components/SocketConnector.tsx
@@ -0,0 +1,27 @@
+import { FC, useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import { usePageListSWR } from '~/stores/page';
+
+export const SocketConnector: FC = () => {
+  const [socket] = useState(() => io(process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000'));
+  const { mutate: pageListMutate } = usePageListSWR();
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('socket connected!!');
+    });
+    socket.on('disconnect', () => {
+      console.log('socket disconnected!!');
+    });
+    socket.on('update-page', () => {
+      console.log('Get Updated Data');
+      pageListMutate();
+    });
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  return null;
+};
