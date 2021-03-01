628880f82412a2bd7d832674cc8db5983bbbaa04
itizawa <itizawa@weseek.co.jp>
2021-03-01 21:52:09 +0900

    implement scroll top

--- src/components/Commons/ScrollTopButton.tsx
+++ src/components/Commons/ScrollTopButton.tsx
@@ -1,9 +1,30 @@
-import { VFC } from 'react';
+import { VFC, useState, useEffect } from 'react';
 
 export const ScrollTopButton: VFC = () => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 1000) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 1000) {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScrollTop);
+    return () => {
+      window.removeEventListener('scroll', checkScrollTop);
+    };
+  }, []);
+
   return (
-    <a id="scroll-to-top" className="btn btn-light btn-lg" role="button">
-      <i className="fas fa-chevron-up"></i>
-    </a>
+    <button id="scroll-to-top" onClick={scrollTop} className="btn btn-light btn-lg">
+      <i className="fas fa-chevron-up" />
+    </button>
   );
 };
