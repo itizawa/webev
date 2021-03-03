1f11e6b862eb37a1981e01be821fb2265bf43478
itizawa <itizawa@weseek.co.jp>
2021-03-03 20:56:26 +0900

    add button color props

--- src/components/Icons/IconButton.tsx
+++ src/components/Icons/IconButton.tsx
@@ -1,4 +1,5 @@
 import { FC, ComponentProps } from 'react';
+import ClassNames from 'classnames';
 import { Icon } from './Icon';
 import { BootstrapColor } from '~/interfaces/variables';
 
@@ -7,11 +8,12 @@ type Props = {
   text?: string;
   color?: BootstrapColor;
   activeColor?: BootstrapColor;
+  buttonColor?: BootstrapColor;
   onClickButton?: () => void;
 } & ComponentProps<typeof Icon>;
 
 export const IconButton: FC<Props> = (props: Props) => {
-  const { width, height, isActive, icon, text, color = BootstrapColor.SECONDARY, activeColor = BootstrapColor.INFO, onClickButton } = props;
+  const { width, height, isActive, icon, text, color = BootstrapColor.SECONDARY, activeColor = BootstrapColor.INFO, buttonColor, onClickButton } = props;
   const textColor = isActive ? activeColor : color;
 
   const handleClickButton = () => {
@@ -20,8 +22,13 @@ export const IconButton: FC<Props> = (props: Props) => {
     }
   };
 
+  const btnClassName = ClassNames({
+    [`btn text-${textColor}`]: true,
+    [`btn-${buttonColor}`]: buttonColor != null,
+  });
+
   return (
-    <button className={`btn text-${textColor}`} onClick={handleClickButton}>
+    <button className={btnClassName} onClick={handleClickButton}>
       <Icon width={width} height={height} icon={icon} />
       {text && <span className="ms-3">{text}</span>}
     </button>
