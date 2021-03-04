98437cecdc8623044a2baec365982b356421c92d
itizawa <itizawa@weseek.co.jp>
2021-03-05 01:37:15 +0900

    impl useActivePage

--- src/stores/page.tsx
+++ src/stores/page.tsx
@@ -3,8 +3,14 @@ import { restClient } from '~/utils/rest-client';
 
 import { PaginationResult } from '~/interfaces/paginationResult';
 import { Page } from '~/interfaces/page';
+import { useStaticSWR } from '~/stores/use-static-swr';
 
-export const usePageListSWR = (activePage = 1, limit = 27, isFavorite = false): responseInterface<PaginationResult<Page>, Error> => {
+export const useActivePage = (initialData?: number): responseInterface<number, Error> => {
+  return useStaticSWR('activePage', initialData);
+};
+
+export const usePageListSWR = (limit = 27, isFavorite = false): responseInterface<PaginationResult<Page>, Error> => {
+  const { data: activePage } = useActivePage();
   let key = `/pages/list?status=stocked&page=${activePage}&limit=${limit}`;
   if (isFavorite) {
     key += `&isFavorite=${isFavorite}`;
