b62be54bd9d23b0e6576c64431be0601c75ec113
itizawa <itizawa@weseek.co.jp>
2021-03-06 19:21:32 +0900

    use LoginRequiredWrapper

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,17 +1,17 @@
 import { VFC } from 'react';
 
+import { LoginRequiredWrapper } from '~/components/Authentication/LogoutRequiredWrapper';
 import { DashBoardLayout } from '~/components/Layout/DashBoardLayout';
 
 const Index: VFC = () => {
   return (
-    <DashBoardLayout>
-      <div className="p-3">
-        <h1>Login Page</h1>
-      </div>
-    </DashBoardLayout>
-
-    // <LoginRequiredWrapper>
-    // </LoginRequiredWrapper>
+    <LoginRequiredWrapper>
+      <DashBoardLayout>
+        <div className="p-3">
+          <h1>Login Page</h1>
+        </div>
+      </DashBoardLayout>
+    </LoginRequiredWrapper>
   );
 };
 
