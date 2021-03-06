d6363d4f6e5bd7cd2cd0c78933956744b44e03fa
itizawa <itizawa@weseek.co.jp>
2021-03-06 20:47:47 +0900

    add alert

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,19 +4,16 @@ import { useRouter } from 'next/router';
 import { signIn } from 'next-auth/client';
 import { LoginRequiredWrapper } from '~/components/Authentication/LogoutRequiredWrapper';
 import { DashBoardLayout } from '~/components/Layout/DashBoardLayout';
-import { toastError } from '~/utils/toastr';
 
 const Index: VFC = () => {
   const router = useRouter();
-  if (router.query.isRedirect) {
-    toastError({ message: 'ログインが必要です' });
-  }
 
   return (
     <LoginRequiredWrapper>
       <DashBoardLayout>
         <div className="p-3">
           <h1>Login Page</h1>
+          {router.query.isRedirect && <div className="alert alert-warning">ログインが必要です</div>}
           <button className="btn btn-primary" onClick={() => signIn('google')}>
             ログイン
           </button>
