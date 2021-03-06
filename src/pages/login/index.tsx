7f1a79a25472ac7e38d1213cfc3d2d8def241bfe
itizawa <itizawa@weseek.co.jp>
2021-03-06 19:42:23 +0900

    add login button

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,6 @@
 import { VFC } from 'react';
 
+import { signIn } from 'next-auth/client';
 import { LoginRequiredWrapper } from '~/components/Authentication/LogoutRequiredWrapper';
 import { DashBoardLayout } from '~/components/Layout/DashBoardLayout';
 
@@ -9,6 +10,9 @@ const Index: VFC = () => {
       <DashBoardLayout>
         <div className="p-3">
           <h1>Login Page</h1>
+          <button className="btn btn-primary" onClick={() => signIn('google')}>
+            ログイン
+          </button>
         </div>
       </DashBoardLayout>
     </LoginRequiredWrapper>
