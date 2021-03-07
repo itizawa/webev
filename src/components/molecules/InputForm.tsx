543377632ebcf53c62a7ec177f10c5624b15e6b5
itizawa <itizawa@weseek.co.jp>
2021-03-07 22:56:57 +0900

    adjust

--- src/components/molecules/InputForm.tsx
+++ src/components/molecules/InputForm.tsx
@@ -32,15 +32,7 @@ export const InputForm: FC = () => {
 
   return (
     <form className="input-group" onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="text"
-        name={urlInputName}
-        ref={register}
-        className={`form-control ps-3 ${styles['left-side']}`}
-        placeholder="URL を保存"
-        aria-label="Input Group"
-        aria-describedby="input-group"
-      />
+      <input type="text" name={urlInputName} ref={register} className={`form-control ps-3 ${styles['left-side']}`} placeholder="URL を保存" />
       <button className={`btn btn-secondary ${styles['right-side']}`} type="submit" id="input-group">
         保存する
       </button>
