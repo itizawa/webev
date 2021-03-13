dcc6088e65de5e3f1849547799db02272f1277d3
itizawa <itizawa@weseek.co.jp>
2021-03-13 10:47:08 +0900

    use UserIcon

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,15 +1,17 @@
 import { signOut, Session } from 'next-auth/client';
 import { VFC } from 'react';
+import { UserIcon } from '~/components/Icons/UserIcon';
 
 type Props = {
   session: Session;
 };
 
 export const ProfileCard: VFC<Props> = (props: Props) => {
-  console.log(props);
+  const { user } = props.session;
   return (
-    <span className="mb-0 text-white c-pointer" onClick={() => signOut()}>
-      Logout
-    </span>
+    <UserIcon />
+    // <span className="mb-0 text-white c-pointer" onClick={() => signOut()}>
+    //   Logout
+    // </span>
   );
 };
