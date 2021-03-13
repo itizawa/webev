e129188aec55ec696507c024bfb445acb0a501a1
itizawa <itizawa@weseek.co.jp>
2021-03-13 18:45:07 +0900

    add env

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ module.exports = {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     MONGO_URI: process.env.MONGO_URI,
+    BASIC_USERNAME: process.env.BASIC_USERNAME,
+    BASIC_PASSWORD: process.env.BASIC_PASSWORD,
   },
   trailingSlash: true,
 };
