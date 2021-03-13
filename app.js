b1f918aa1cf35ac17ce979d41c6d783e4e3f751f
itizawa <itizawa@weseek.co.jp>
2021-03-13 18:47:15 +0900

    adjust

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const safeCompare = require('safe-compare');
 
 const app = protect(
   '/',
-  (username, password) => {
-    safeCompare(username, process.env.BASIC_USERNAME || 'admin') && safeCompare(password, process.env.BASIC_PASSWORD || 'admin')
-  },
+  (username, password) => safeCompare(username, process.env.BASIC_USERNAME || 'admin') && safeCompare(password, process.env.BASIC_PASSWORD || 'admin'),
   {
     directory: `${__dirname}/out`,
     onAuthFailed: (res) => {
