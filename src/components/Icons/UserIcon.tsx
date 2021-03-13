8f4efa0957dd879ee5870b1f5393284bd7fa9f7d
itizawa <itizawa@weseek.co.jp>
2021-03-13 15:18:15 +0900

    add size to props

--- src/components/Icons/UserIcon.tsx
+++ src/components/Icons/UserIcon.tsx
@@ -1,9 +1,24 @@
 import { VFC } from 'react';
 
 type Props = {
-  image?: string;
+  image: string;
+  size: 'small' | 'medium' | 'large';
 };
 
 export const UserIcon: VFC<Props> = (props: Props) => {
-  return <img src={props.image || 'no'} />;
+  const { size } = props;
+  let height;
+  switch (size) {
+    case 'small':
+      height = '16px';
+      break;
+    case 'medium':
+      height = '32px';
+      break;
+    case 'large':
+      height = '46px';
+      break;
+  }
+
+  return <img height={height} width="auto" src={props.image} />;
 };
