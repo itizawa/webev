02355e09a6fa215b19871423df1a9da9d2e20985
itizawa <itizawa@weseek.co.jp>
2021-03-15 20:55:26 +0900

    add  JSX.Element

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  render() {
+  render(): JSX.Element {
     return (
       <Html lang="ja">
         <Head>
