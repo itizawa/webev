e0df8dc7fe3c8c9dec7a88896d059674b0f9f16f
itizawa <itizawa@weseek.co.jp>
2021-03-15 20:53:48 +0900

    replace deprecate method with new one

--- src/stores/modal.tsx
+++ src/stores/modal.tsx
@@ -1,11 +1,11 @@
-import { responseInterface } from 'swr';
+import { SWRResponse } from 'swr';
 import { Page } from '~/interfaces/page';
 import { useStaticSWR } from '~/stores/use-static-swr';
 
-export const usePageForDelete = (initialData?: Page): responseInterface<Page, Error> => {
+export const usePageForDelete = (initialData?: Page): SWRResponse<Page, Error> => {
   return useStaticSWR('pageForDeletePageModal', initialData);
 };
 
-export const useIsOpenDeletePageModal = (initialData?: boolean): responseInterface<boolean, Error> => {
+export const useIsOpenDeletePageModal = (initialData?: boolean): SWRResponse<boolean, Error> => {
   return useStaticSWR('isOpenDeletePageModal', initialData);
 };
--- src/stores/page.tsx
+++ src/stores/page.tsx
@@ -1,19 +1,19 @@
-import useSWR, { responseInterface } from 'swr';
+import useSWR, { SWRResponse } from 'swr';
 import { restClient } from '~/utils/rest-client';
 
 import { PaginationResult } from '~/interfaces/paginationResult';
 import { Page } from '~/interfaces/page';
 import { useStaticSWR } from '~/stores/use-static-swr';
 
-export const useActivePage = (initialData?: number): responseInterface<number, Error> => {
+export const useActivePage = (initialData?: number): SWRResponse<number, Error> => {
   return useStaticSWR('activePage', initialData);
 };
 
-export const useIsRetrieveFavoritePageList = (initialData?: boolean): responseInterface<boolean, Error> => {
+export const useIsRetrieveFavoritePageList = (initialData?: boolean): SWRResponse<boolean, Error> => {
   return useStaticSWR('isRetrieveFavoritePageList', initialData);
 };
 
-export const usePageListSWR = (limit = 27): responseInterface<PaginationResult<Page>, Error> => {
+export const usePageListSWR = (limit = 27): SWRResponse<PaginationResult<Page>, Error> => {
   const { data: activePage = 1 } = useActivePage();
   const { data: isRetrieveFavoritePageList = false } = useIsRetrieveFavoritePageList();
   let key = `/pages/list?status=stocked&page=${activePage}&limit=${limit}`;
