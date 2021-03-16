1c5881da2b5a7c660c7c3c26653cf458f6228400
itizawa <itizawa@weseek.co.jp>
2021-03-16 22:38:01 +0900

    create DefaultLayout

---
+++ src/components/Layout/DefaultLayout.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import { FC } from 'react';
+import { useSession } from 'next-auth/client';
+
+import { SocketConnector } from '~/components/SocketConnector';
+
+import style from '~/styles/navbarBorder.module.scss';
+
+import { Navbar } from '~/components/organisms/Navbar';
+import { PageModals } from '~/components/PageModals/PageModals';
+import { ScrollTopButton } from '~/components/Commons/ScrollTopButton';
+
+export const DefaultLayout: FC = ({ children }) => {
+  const [session] = useSession();
+
+  return (
+    <>
+      <Head>
+        <title>Webev</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <Navbar />
+      <div className={`webev-nav-border ${style['nav-border']}`} />
+      <main>
+        <div className="container">{children}</div>
+      </main>
+      {session && <PageModals />}
+      {session && <SocketConnector />}
+      <ScrollTopButton />
+    </>
+  );
+};
