84161419606b85fe14be51bcbdc636131b73539d
itizawa <itizawa@weseek.co.jp>
2021-03-17 21:28:05 +0900

    adjust input form

--- src/components/molecules/InputForm.tsx
+++ src/components/molecules/InputForm.tsx
@@ -1,24 +1,16 @@
-import { FC } from 'react';
-import { useForm } from 'react-hook-form';
-
-import styles from '~/styles/components/molecules/InputForm.module.scss';
+import { FC, useState } from 'react';
 
 import { restClient } from '~/utils/rest-client';
 import { toastError, toastSuccess } from '~/utils/toastr';
 import { usePageListSWR } from '~/stores/page';
 
-type FormValues = {
-  url: string;
-};
-
-const urlInputName = 'url';
-
 export const InputForm: FC = () => {
-  const { register, handleSubmit, setValue } = useForm<FormValues>();
   const { mutate: mutatePageList } = usePageListSWR();
 
-  const onSubmit = async (formValues: FormValues): Promise<void> => {
-    const { url } = formValues;
+  const [url, setUrl] = useState('');
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
 
     try {
       await restClient.apiPost('/pages', { url });
@@ -27,13 +19,12 @@ export const InputForm: FC = () => {
     } catch (err) {
       toastError(err);
     }
-    setValue(urlInputName, '');
   };
 
   return (
-    <form className="input-group" onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" name={urlInputName} ref={register} className={`form-control ps-3 ${styles['left-side']}`} placeholder="URL を保存" />
-      <button className={`btn btn-secondary ${styles['right-side']}`} type="submit" id="input-group">
+    <form className="input-group" onSubmit={onSubmit}>
+      <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} className="form-control ps-3" placeholder="URL を保存" />
+      <button className="btn btn-secondary" type="submit" id="input-group">
         保存する
       </button>
     </form>
