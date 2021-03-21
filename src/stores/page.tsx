e836ebf45c8d66b0072ad42d864d77f3fa50e3d0
itizawa <itizawa@weseek.co.jp>
2021-03-21 20:37:02 +0900

    refactor key

--- src/stores/page.tsx
+++ src/stores/page.tsx
@@ -1,6 +1,7 @@
 import useSWR, { SWRResponse } from 'swr';
-import { restClient } from '~/utils/rest-client';
+import urljoin from 'url-join';
 
+import { restClient } from '~/utils/rest-client';
 import { PaginationResult } from '~/interfaces/paginationResult';
 import { Page } from '~/interfaces/page';
 import { useStaticSWR } from '~/stores/use-static-swr';
@@ -16,12 +17,14 @@ export const useIsRetrieveFavoritePageList = (initialData?: boolean): SWRRespons
 export const usePageListSWR = (limit = 27): SWRResponse<PaginationResult<Page>, Error> => {
   const { data: activePage = 1 } = useActivePage();
   const { data: isRetrieveFavoritePageList = false } = useIsRetrieveFavoritePageList();
-  let key = `/pages/list?status=stocked&page=${activePage}&limit=${limit}`;
-  if (isRetrieveFavoritePageList) {
-    key += `&isFavorite=${isRetrieveFavoritePageList}`;
-  }
-  return useSWR(key, (endpoint) => restClient.apiGet(endpoint).then((result) => result.data), {
-    revalidateOnFocus: false,
-    revalidateOnReconnect: true,
-  });
+
+  return useSWR(
+    ['/pages/list?status=stocked', activePage, limit, isRetrieveFavoritePageList],
+    (endpoint, page, limit, isFavorite) =>
+      restClient.apiGet(urljoin(endpoint, `?page=${page}`, `&limit=${limit}`, isFavorite ? `&isFavorite=${isFavorite}` : ``)).then((result) => result.data),
+    {
+      revalidateOnFocus: false,
+      revalidateOnReconnect: true,
+    },
+  );
 };
