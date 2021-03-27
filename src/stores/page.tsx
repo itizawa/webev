aaf00a2f59128e9549246b0f011d8b1a6daa4bdd
itizawa <itizawa@weseek.co.jp>
2021-03-28 00:16:47 +0900

    implement useIsSortCreatedAt

--- src/stores/page.tsx
+++ src/stores/page.tsx
@@ -15,6 +15,10 @@ export const useIsRetrieveFavoritePageList = (initialData?: boolean): SWRRespons
   return useStaticSWR('isRetrieveFavoritePageList', initialData);
 };
 
+export const useIsSortCreatedAt = (initialData?: boolean): SWRResponse<boolean, Error> => {
+  return useStaticSWR('isSortCreatedAt', initialData);
+};
+
 export const usePageListSWR = (limit = 27): SWRResponse<PaginationResult<Page>, Error> => {
   const { data: activePage = 1 } = useActivePage();
   const { data: isRetrieveFavoritePageList = false } = useIsRetrieveFavoritePageList();
