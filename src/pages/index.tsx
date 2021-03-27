8cd30a9d87164feb6274eaa6a31942fcd477a4c2
itizawa <itizawa@weseek.co.jp>
2021-03-27 21:10:35 +0900

    update top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,19 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
+import { useTranslation } from 'react-i18next';
 import { DefaultLayout } from '~/components/Layout/DefaultLayout';
 
 const Index: VFC = () => {
+  const { t } = useTranslation();
+
   return (
     <DefaultLayout>
-      <h1 className="my-3">Welcome Webev!!</h1>
+      <h1 className="my-3">{t('welcome_webev')}</h1>
       <Image src="/images/eye-catch.png" height={1260} width={2240} />
       <div className="text-center">
         <Link href="/home">
-          <button className="btn btn-info text-white mt-3">さっそく 始める</button>
+          <button className="btn btn-info text-white mt-3">{t('start_immediately')}</button>
         </Link>
       </div>
     </DefaultLayout>
