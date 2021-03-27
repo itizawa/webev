0043a41ce180bf521e37bbda15135f1517048b6c
itizawa <itizawa@weseek.co.jp>
2021-03-27 21:10:07 +0900

    update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,15 +2,17 @@ import { VFC } from 'react';
 import Link from 'next/link';
 
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'react-i18next';
 import { DefaultLayout } from '~/components/Layout/DefaultLayout';
 
 const Page: VFC = () => {
+  const { t } = useTranslation();
   return (
     <DefaultLayout>
       <div className="p-3">
-        <h1 className="m-3">This is the 404 page</h1>
+        <h1 className="m-3">{t('this_is_the_404_page')}</h1>
         <h2>
-          <Link href="/home">Go to Home</Link>
+          <Link href="/">{t('go_to_top')}</Link>
         </h2>
       </div>
     </DefaultLayout>
