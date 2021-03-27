129168752e8f5c12df2ceb5333c26ef77b65fc9f
itizawa <itizawa@weseek.co.jp>
2021-03-27 19:52:14 +0900

    adjust

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,4 +1,7 @@
-export const i18n = {
-  defaultLocale: 'en',
-  locales: ['en', 'ja'],
+// eslint-disable-next-line no-undef
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ja'],
+  },
 };
