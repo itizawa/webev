0b9b8ad7c4a4499b27277cd2444e95f9dc9f926d
itizawa <itizawa@weseek.co.jp>
2021-04-04 22:15:40 +0900

    adjust bg color

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 export const Footer: FC = () => {
   return (
-    <footer className="footer mt-auto py-3 bg-light">
+    <footer className="footer mt-5 py-3 bg-dark border-top border-secondary">
       <div className="container">
         <span className="text-muted">Place sticky footer content here.</span>
       </div>
