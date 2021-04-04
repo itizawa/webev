3229b531faa4b40bbedefb30c261b23cb84a89f1
itizawa <itizawa@weseek.co.jp>
2021-04-04 22:27:58 +0900

    adjust style

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,10 +1,18 @@
+import Link from 'next/link';
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const Footer: FC = () => {
+  const { t } = useTranslation();
+
   return (
     <footer className="footer mt-5 py-3 bg-dark border-top border-secondary">
       <div className="container">
-        <span className="text-muted">Place sticky footer content here.</span>
+        <h5 className="text-muted">Webev</h5>
+        <br />
+        <Link href="/term">
+          <a className="text-muted">{t('term')}</a>
+        </Link>
       </div>
     </footer>
   );
