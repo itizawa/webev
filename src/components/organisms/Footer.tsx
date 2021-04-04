52e159b118cb4331367602bdf845783ce3d0e1cb
itizawa <itizawa@weseek.co.jp>
2021-04-04 22:03:11 +0900

    create footer component

---
+++ src/components/organisms/Footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+export const Footer: FC = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-light">
+      <div className="container">
+        <span className="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+  );
+};
