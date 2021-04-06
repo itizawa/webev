34d9a90bd35cd542aa334834e0f2c249a79f1816
itizawa <itizawa@weseek.co.jp>
2021-04-06 23:10:32 +0900

    display PLUS_DOTTED icon

--- src/components/Sidebar/Diectory.tsx
+++ src/components/Sidebar/Diectory.tsx
@@ -1,17 +1,23 @@
 import { VFC } from 'react';
 import Link from 'next/link';
 
+import { IconButton } from '../Icons/IconButton';
 import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
 
 import { Icon } from '~/components/Icons/Icon';
 
 export const Diectory: VFC = () => {
   return (
-    <h5 className="text-center">
-      <Icon icon={BootstrapIcon.DIRECTORY} color={BootstrapColor.LIGHT} />
-      <Link href="/directory">
-        <span className="ms-2 d-none d-lg-inline-block c-pointer">Directory</span>
-      </Link>
-    </h5>
+    <>
+      <h5 className="text-center">
+        <Icon icon={BootstrapIcon.DIRECTORY} color={BootstrapColor.LIGHT} />
+        <Link href="/directory">
+          <span className="ms-2 d-none d-lg-inline-block c-pointer">Directory</span>
+        </Link>
+      </h5>
+      <div className="text-center">
+        <IconButton icon={BootstrapIcon.PLUS_DOTTED} color={BootstrapColor.LIGHT} activeColor={BootstrapColor.LIGHT} />
+      </div>
+    </>
   );
 };
