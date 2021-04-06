3f0ba0c8d31ba9a793316a7861f0c6fb0d348f28
itizawa <itizawa@weseek.co.jp>
2021-04-06 23:17:47 +0900

    adjust style

--- src/components/Sidebar/Diectory.tsx
+++ src/components/Sidebar/Diectory.tsx
@@ -1,5 +1,6 @@
 import { VFC } from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
 
 import { IconButton } from '~/components/Icons/IconButton';
 import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
@@ -15,9 +16,20 @@ export const Diectory: VFC = () => {
           <span className="ms-2 d-none d-lg-inline-block c-pointer">Directory</span>
         </Link>
       </h5>
-      <div className="text-center">
+      <StyledDiv className="text-center mx-3">
         <IconButton icon={BootstrapIcon.PLUS_DOTTED} color={BootstrapColor.LIGHT} activeColor={BootstrapColor.LIGHT} />
-      </div>
+      </StyledDiv>
     </>
   );
 };
+
+const StyledDiv = styled.div`
+  .btn {
+    width: 100%;
+    border-radius: 3px;
+    :hover {
+      background-color: rgba(200, 200, 200, 0.2);
+      transition: all 300ms linear;
+    }
+  }
+`;
