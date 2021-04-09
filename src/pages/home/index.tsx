65d4bea53eb61501a7db796782634d003fbcce1c
itizawa <itizawa@weseek.co.jp>
2021-04-09 23:51:53 +0900

    display favorite button

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,9 @@ import { VFC } from 'react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-import { usePageListSWR } from '~/stores/page';
+import { useIsRetrieveFavoritePageList, usePageListSWR } from '~/stores/page';
+
+import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
 
 import { OgpCard } from '~/components/organisms/OgpCard';
 import { LoginRequiredWrapper } from '~/components/Authentication/LoginRequiredWrapper';
@@ -11,11 +13,13 @@ import { DashBoardLayout } from '~/components/Layout/DashBoardLayout';
 import { PaginationWrapper } from '~/components/Commons/PaginationWrapper';
 import { SortButtonGroup } from '~/components/Commons/SortButtonGroup';
 import { NoPageAlert } from '~/components/Alerts/NoPageAlert';
+import { IconButton } from '~/components/Icons/IconButton';
 
 const Index: VFC = () => {
   const { t } = useTranslation();
 
   const { data: paginationResult } = usePageListSWR();
+  const { data: isRetrieveFavoritePageList, mutate: mutateIsRetrieveFavoritePageList } = useIsRetrieveFavoritePageList();
 
   return (
     <LoginRequiredWrapper>
@@ -27,7 +31,18 @@ const Index: VFC = () => {
               <span className="badge rounded-pill bg-secondary">{paginationResult?.totalDocs} Pages</span>
             </div>
           </div>
-          <div className="my-2 d-flex flex-row-reverse ms-auto">
+          <div className="my-2 d-flex">
+            <div className="ms-auto me-3">
+              <IconButton
+                icon={BootstrapIcon.STAR}
+                isActive={isRetrieveFavoritePageList}
+                color={BootstrapColor.SECONDARY}
+                activeColor={BootstrapColor.WARNING}
+                onClickButton={() => mutateIsRetrieveFavoritePageList(!isRetrieveFavoritePageList)}
+                buttonSize="sm"
+                text={t('only_favorite')}
+              />
+            </div>
             <SortButtonGroup />
           </div>
           {paginationResult != null && (
