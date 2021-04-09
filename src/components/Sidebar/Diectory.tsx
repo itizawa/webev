bb5f6eee281b3fa16f8bcc8145f72c61878bc366
itizawa <itizawa@weseek.co.jp>
2021-04-09 23:18:41 +0900

    implement post directory

--- src/components/Sidebar/Diectory.tsx
+++ src/components/Sidebar/Diectory.tsx
@@ -2,6 +2,11 @@ import { useState, VFC } from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
 
+import { useTranslation } from 'react-i18next';
+import { restClient } from '~/utils/rest-client';
+
+import { toastError, toastSuccess } from '~/utils/toastr';
+
 import { IconButton } from '~/components/Icons/IconButton';
 import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
 
@@ -9,10 +14,19 @@ import { Icon } from '~/components/Icons/Icon';
 
 export const Diectory: VFC = () => {
   const [isCreatingNewDirectory, setIsCreatingNewDirectory] = useState(false);
-  const [directoryName, setDirectoryName] = useState('');
+  const [name, setName] = useState('');
+  const { t } = useTranslation();
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
+
+    try {
+      await restClient.apiPost('/directories', { name });
+      toastSuccess(t('toastr.save', { target: 'Directory' }));
+      setName('');
+    } catch (err) {
+      toastError(err);
+    }
 
     setIsCreatingNewDirectory(false);
   };
@@ -28,7 +42,7 @@ export const Diectory: VFC = () => {
       <StyledDiv className="text-center mx-3">
         {isCreatingNewDirectory ? (
           <form className="input-group" onSubmit={onSubmit}>
-            <input type="text" value={directoryName} onChange={(e) => setDirectoryName(e.target.value)} className="form-control bg-white" placeholder="...name" />
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="form-control bg-white" placeholder="...name" />
           </form>
         ) : (
           <IconButton
