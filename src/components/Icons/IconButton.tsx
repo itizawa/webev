9a8329d1a264c6ff924abf798c6a37fbda7fcb82
itizawa <itizawa@weseek.co.jp>
2021-04-09 23:45:02 +0900

    add button size props

--- src/components/Icons/IconButton.tsx
+++ src/components/Icons/IconButton.tsx
@@ -8,12 +8,13 @@ type Props = {
   text?: string;
   color: BootstrapColor;
   activeColor: BootstrapColor;
+  buttonSize?: 'sm' | 'lg';
   buttonColor?: BootstrapColor;
   onClickButton?: () => void;
 } & ComponentProps<typeof Icon>;
 
 export const IconButton: VFC<Props> = (props: Props) => {
-  const { width, height, isActive, disabled, icon, text, color, activeColor, buttonColor, onClickButton } = props;
+  const { width, height, isActive, disabled, icon, text, color, activeColor, buttonColor, buttonSize, onClickButton } = props;
   const textColor = isActive ? activeColor : color;
 
   const handleClickButton = () => {
@@ -23,6 +24,9 @@ export const IconButton: VFC<Props> = (props: Props) => {
   };
 
   const btnClassName: string[] = ['btn', 'px-2'];
+  if (buttonSize != null) {
+    btnClassName.push(`btn-${buttonSize}`);
+  }
   if (buttonColor != null) {
     btnClassName.push(`btn-${buttonColor}`);
   }
