7f04f0cd4ef003a9205c1e42abccb35703091ded
itizawa <itizawa@weseek.co.jp>
2021-04-10 01:23:45 +0900

    display list

--- src/components/Sidebar/Diectory.tsx
+++ src/components/Sidebar/Diectory.tsx
@@ -11,8 +11,11 @@ import { IconButton } from '~/components/Icons/IconButton';
 import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
 
 import { Icon } from '~/components/Icons/Icon';
+import { useDirectoryListSWR } from '~/stores/directory';
 
 export const Diectory: VFC = () => {
+  const { data: paginationResult } = useDirectoryListSWR();
+
   const [isCreatingNewDirectory, setIsCreatingNewDirectory] = useState(false);
   const [name, setName] = useState('');
   const { t } = useTranslation();
@@ -40,6 +43,17 @@ export const Diectory: VFC = () => {
         </Link>
       </h5>
       <StyledDiv className="text-center mx-3">
+        <ul className="sidebar-list-group list-group gap-3 py-3">
+          {paginationResult?.docs.map((v) => {
+            return (
+              <Link key={v._id} href={`/directories/${v._id}`}>
+                <StyledList className="list-group-item border-0">
+                  <span>{v.name}</span>
+                </StyledList>
+              </Link>
+            );
+          })}
+        </ul>
         {isCreatingNewDirectory ? (
           <form className="input-group" onSubmit={onSubmit}>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="form-control bg-white" placeholder="...name" />
@@ -57,9 +71,22 @@ export const Diectory: VFC = () => {
   );
 };
 
+const StyledList = styled.li`
+  padding: 10px;
+  color: #eee;
+  background-color: inherit;
+  border-radius: 3px;
+
+  :hover {
+    background-color: rgba(200, 200, 200, 0.2);
+    transition: all 300ms linear;
+  }
+`;
+
 const StyledDiv = styled.div`
   > .btn {
     width: 100%;
+    padding: 10px;
     border-radius: 3px;
     :hover {
       background-color: rgba(200, 200, 200, 0.2);
