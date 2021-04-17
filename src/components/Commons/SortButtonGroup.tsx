fba21e0aac811593d04fa9370d56a3c595142e60
itizawa <itizawa@weseek.co.jp>
2021-04-17 21:22:13 +0900

    update i18n

--- src/components/Commons/SortButtonGroup.tsx
+++ src/components/Commons/SortButtonGroup.tsx
@@ -1,20 +1,20 @@
 import { VFC } from 'react';
-import { useTranslation } from 'react-i18next';
 
+import { useLocale } from '~/hooks/useLocale';
 import { useIsSortCreatedAt } from '~/stores/page';
 
 export const SortButtonGroup: VFC = () => {
-  const { t } = useTranslation();
+  const { t } = useLocale();
 
   const { data: isSortCreatedAt, mutate: mutateIsSortCreatedAt } = useIsSortCreatedAt();
 
   return (
     <div className="btn-group btn-group-sm" role="group">
       <button className={`btn btn-outline-indigo text-white ${!isSortCreatedAt ? 'active' : ''}`} onClick={() => mutateIsSortCreatedAt(false)}>
-        {t('latest_order')}
+        {t.latest_order}
       </button>
       <button className={`btn btn-outline-indigo text-white ${isSortCreatedAt ? 'active' : ''}`} onClick={() => mutateIsSortCreatedAt(true)}>
-        {t('oldest_order')}
+        {t.oldest_order}
       </button>
     </div>
   );
