dc3ae2b715d8b735c1c29be3da2a08ce4e5b130e
itizawa <itizawa@weseek.co.jp>
2021-04-17 21:17:42 +0900

    update i18n

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,35 @@
 import { ReactNode } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import CountUp from 'react-countup';
-import { useTranslation } from 'react-i18next';
 import axios from 'axios';
+
 import { DefaultLayout } from '~/components/Layout/DefaultLayout';
+import { useLocale } from '~/hooks/useLocale';
 
 type Props = {
   count: number;
 };
 
 const Index: ReactNode = (props: Props) => {
-  const { t } = useTranslation();
+  const { t } = useLocale();
 
   return (
     <DefaultLayout>
-      <h1 className="my-3">{t('welcome_webev')}</h1>
+      <h1 className="my-3">{t.welcome_webev}</h1>
       <Image src="/images/eye-catch-dark.png" height={1260} width={2240} />
       <div className="row my-3">
         <div className="card bg-dark border border-warning text-white p-3 col-6 offset-3 text-center">
           <h2>
             <CountUp end={props.count} delay={1} /> Pages
           </h2>
-          <p>{t('total_pages')}</p>
+          <p>{t.total_pages}</p>
         </div>
       </div>
       <div className="text-center">
         <Link href="/home">
-          <button className="btn btn-purple mt-3">{t('start_immediately')}</button>
+          <button className="btn btn-purple mt-3">{t.start_immediately}</button>
         </Link>
       </div>
     </DefaultLayout>
@@ -37,14 +37,13 @@ const Index: ReactNode = (props: Props) => {
 };
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const getStaticProps = async ({ locale }: { locale: string }) => {
+export const getStaticProps = async () => {
   const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/pages/all`);
   const count = res.data;
 
   return {
     props: {
       count,
-      ...(await serverSideTranslations(locale, ['common'])),
     },
   };
 };
