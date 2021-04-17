7ca96d59ee549a25330786738049752330eab233
itizawa <itizawa@weseek.co.jp>
2021-04-17 20:28:04 +0900

    display count

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,29 @@
-import { VFC } from 'react';
+import { ReactNode } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { useTranslation } from 'react-i18next';
 import { DefaultLayout } from '~/components/Layout/DefaultLayout';
+import { restClient } from '~/utils/rest-client';
 
-const Index: VFC = () => {
+type Props = {
+  count: number;
+};
+
+const Index: ReactNode = (props: Props) => {
   const { t } = useTranslation();
 
   return (
     <DefaultLayout>
       <h1 className="my-3">{t('welcome_webev')}</h1>
       <Image src="/images/eye-catch-dark.png" height={1260} width={2240} />
+      <div className="row my-3">
+        <div className="card bg-dark text-white p-3 col-6 offset-3 text-center">
+          <h2>{props.count}</h2>
+          <p>Total pages saved using webev!</p>
+        </div>
+      </div>
       <div className="text-center">
         <Link href="/home">
           <button className="btn btn-purple mt-3">{t('start_immediately')}</button>
@@ -23,10 +34,16 @@ const Index: VFC = () => {
 };
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const getStaticProps = async ({ locale }: { locale: string }) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ['common'])),
-  },
-});
+export const getStaticProps = async ({ locale }: { locale: string }) => {
+  const res = await restClient.apiGet('/pages/all');
+  const count = res.data;
+
+  return {
+    props: {
+      count,
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  };
+};
 
 export default Index;
