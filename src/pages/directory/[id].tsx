242ae4fe28dcfb624562a279b7fe6b26f3c44f07
itizawa <itizawa@weseek.co.jp>
2021-04-17 22:55:11 +0900

    display name

--- src/pages/directory/[id].tsx
+++ src/pages/directory/[id].tsx
@@ -4,21 +4,23 @@ import { useRouter } from 'next/router';
 import { LoginRequiredWrapper } from '~/components/Authentication/LoginRequiredWrapper';
 import { DashBoardLayout } from '~/components/Layout/DashBoardLayout';
 
-import { usePageListByDirectoryId } from '~/stores/directory';
+import { useDirectoryInfomation, usePageListByDirectoryId } from '~/stores/directory';
 import { OgpCard } from '~/components/organisms/OgpCard';
 
 const Index: VFC = () => {
   const router = useRouter();
   const { id } = router.query;
 
+  const { data: directory } = useDirectoryInfomation(id as string);
   const { data: pages } = usePageListByDirectoryId(id as string);
 
   return (
     <LoginRequiredWrapper>
       <DashBoardLayout>
         <div className="p-3">
-          {/* TODO display directory info */}
-          <div className="d-flex align-items-center">{/* <h1>{directory?.name}</h1> */}</div>
+          <div className="d-flex align-items-center">
+            <h1>{directory?.name}</h1>
+          </div>
           {pages != null && (
             <div className="row">
               {pages.map((page) => (
