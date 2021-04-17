84ca5fc066c9f44eb39339756500eb74f8818adc
itizawa <itizawa@weseek.co.jp>
2021-04-17 21:17:21 +0900

    create new file

---
+++ src/locales/en.ts
@@ -0,0 +1,48 @@
+export const en = {
+  welcome_webev: 'Welcome Webev!!',
+  total_pages: 'Total pages saved using Webev!',
+  start_immediately: 'Start!',
+  term: 'Terms of Service',
+
+  logout: 'Logout',
+  delete: 'Delete',
+  cancel: 'Cancel',
+  settings: 'Settings',
+  save: 'Save',
+
+  only_favorite: 'Only Favorite',
+  latest_order: 'Latest Order',
+  oldest_order: 'Oldest Order',
+
+  toastr_update: 'Update {{target}}',
+  toastr_delete: 'Delete {{target}}',
+  toastr_save: 'Save {{target}}',
+  toastr_success_archived: 'Archived',
+  toastr_success_add_directory: 'Add Directory',
+
+  delete_favorite_page: 'Delete favorite page',
+  delete_favorite_page_desc: 'Once deleted, it cannot be revert.',
+
+  home: 'Home',
+  favorite: 'Favorite',
+  archive: 'Archive',
+  directory: 'Directory',
+
+  automatic_acquisition: 'Automatic acquisition',
+  function_details: 'function details',
+
+  delete_page: 'Delete Page',
+  obtained_from_clipboard: 'Obtained from Clipboard',
+
+  add_directory: 'Add Directory',
+
+  'your_pages_don’t_exist': 'Your pages don’t exist.',
+  save_the_url_immediately: 'Please add them from the top bar.',
+  'your_favorite_pages_don’t_exist': 'Your favorite pages don’t exist.',
+  favorite_immediately: 'Let’s try to add favorites!',
+  'your_archive_pages_don’t_exist': 'Your archive pages don’t exist.',
+  archive_immediately: 'Let’s try to add archives!',
+
+  this_is_the_404_page: 'This is the 404 page',
+  go_to_top: 'Go to Top Page',
+};
