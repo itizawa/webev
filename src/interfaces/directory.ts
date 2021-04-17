c453f0760121daead45c1815b179544a981f2abd
itizawa <itizawa@weseek.co.jp>
2021-04-17 19:37:40 +0900

    adjust domain

--- src/interfaces/directory.ts
+++ src/interfaces/directory.ts
@@ -1,18 +1,14 @@
-import { Page } from './page';
-
 export class Directory {
   _id: string;
   name: string;
   order: number;
-  pages: Page[];
   createdUser: string;
   createdAt: Date;
   updatedAt: Date;
-  constructor(_id: string, name: string, order: number, pages: Page[], createdUser: string, createdAt: Date, updatedAt: Date) {
+  constructor(_id: string, name: string, order: number, createdUser: string, createdAt: Date, updatedAt: Date) {
     this._id = _id;
     this.name = name;
     this.order = order;
-    this.pages = pages;
     this.createdUser = createdUser;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
