8923161b20e20f0eb0feabceaa01e90668bfe69e
itizawa <itizawa@weseek.co.jp>
2021-04-18 00:13:21 +0900

    implement useDirectoryId

--- src/stores/page.tsx
+++ src/stores/page.tsx
@@ -19,6 +19,10 @@ export const useIsRetrieveFavoritePageList = (initialData?: boolean): SWRRespons
   return useStaticSWR('isRetrieveFavoritePageList', initialData);
 };
 
+export const useDirectoryId = (initialData?: boolean): SWRResponse<boolean, Error> => {
+  return useStaticSWR('directoryId', initialData);
+};
+
 export const useIsSortCreatedAt = (initialData?: boolean): SWRResponse<boolean, Error> => {
   return useStaticSWR('isSortCreatedAt', initialData);
 };
@@ -27,15 +31,26 @@ export const usePageListSWR = (limit = 27): SWRResponse<PaginationResult<Page>, 
   const { data: activePage = 1 } = useActivePage();
   const { data: status = PageStatus.PAGE_STATUS_STOCK } = usePageStatus();
   const { data: isRetrieveFavoritePageList = false } = useIsRetrieveFavoritePageList();
+  const { data: directoryId } = useDirectoryId();
   const { data: isSortCreatedAt = false } = useIsSortCreatedAt();
 
   const sort = isSortCreatedAt ? 'createdAt' : '-createdAt';
 
   return useAuthenticationSWR(
-    ['/pages/list', status, activePage, limit, sort, isRetrieveFavoritePageList],
-    (endpoint, status, page, limit, sort, isFavorite) =>
+    ['/pages/list', status, activePage, limit, sort, isRetrieveFavoritePageList, directoryId],
+    (endpoint, status, page, limit, sort, isFavorite, directoryId) =>
       restClient
-        .apiGet(urljoin(endpoint, `?status=${status}`, `?page=${page}`, `&limit=${limit}`, `&sort=${sort}`, isFavorite ? `&isFavorite=${isFavorite}` : ``))
+        .apiGet(
+          urljoin(
+            endpoint,
+            `?status=${status}`,
+            `?page=${page}`,
+            `&limit=${limit}`,
+            `&sort=${sort}`,
+            isFavorite ? `&isFavorite=${isFavorite}` : ``,
+            directoryId ? `&directoryId=${directoryId}` : ``,
+          ),
+        )
         .then((result) => result.data),
     {
       revalidateOnFocus: false,
