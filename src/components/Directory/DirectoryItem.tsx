65c1222fc1dfdca76e4d075ad799cdfe2dde9679
itizawa <itizawa@weseek.co.jp>
2021-04-25 01:57:42 +0900

    adjust style

--- src/components/Directory/DirectoryItem.tsx
+++ src/components/Directory/DirectoryItem.tsx
@@ -1,6 +1,8 @@
 import { useState, VFC } from 'react';
 import { Collapse } from 'reactstrap';
 
+import styled from 'styled-components';
+
 import { IconButton } from '~/components/Icons/IconButton';
 import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
 
@@ -10,15 +12,36 @@ export const DirectoryItem: VFC = (props: Props) => {
 
   return (
     <>
-      <div className="text-white text-left w-100 rounded" role="button" onClick={() => setIsOpen((prevState) => !prevState)}>
+      <StyledDiv className="text-white text-left w-100 rounded" role="button" onClick={() => setIsOpen((prevState) => !prevState)}>
         {isOpen ? (
-          <IconButton width={18} height={18} icon={BootstrapIcon.CARET_DOWN} color={BootstrapColor.SECONDARY} activeColor={BootstrapColor.SECONDARY} />
+          <IconButton
+            width={18}
+            height={18}
+            icon={BootstrapIcon.CARET_DOWN}
+            color={BootstrapColor.SECONDARY}
+            activeColor={BootstrapColor.SECONDARY}
+            isRemovePadding
+          />
         ) : (
-          <IconButton width={18} height={18} icon={BootstrapIcon.CARET_RIGHT} color={BootstrapColor.SECONDARY} activeColor={BootstrapColor.SECONDARY} />
+          <IconButton
+            width={18}
+            height={18}
+            icon={BootstrapIcon.CARET_RIGHT}
+            color={BootstrapColor.SECONDARY}
+            activeColor={BootstrapColor.SECONDARY}
+            isRemovePadding
+          />
         )}
         hoge
-      </div>
+      </StyledDiv>
       <Collapse isOpen={isOpen}>{isOpen && <DirectoryItem />}</Collapse>
     </>
   );
 };
+
+const StyledDiv = styled.div`
+  :hover {
+    background-color: rgba(200, 200, 200, 0.2);
+    transition: all 300ms linear;
+  }
+`;
