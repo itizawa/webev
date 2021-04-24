38e98a033bd3a9e8f8cbab8f223a208a24758ff7
itizawa <itizawa@weseek.co.jp>
2021-04-24 20:16:20 +0900

    create pencil icon

--- src/components/Icons/Icon.tsx
+++ src/components/Icons/Icon.tsx
@@ -34,6 +34,10 @@ const StarIcon: VFC = () => (
   <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
 );
 
+const PencilIcon: VFC = () => (
+  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+);
+
 const TrashIcon: VFC = () => (
   <>
     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
@@ -130,6 +134,7 @@ const IconComponentMapping = {
   [BootstrapIcon.PLUS_DOTTED]: <PlusDottedIcon />,
   [BootstrapIcon.ARCHIVE]: <ArchiveIcon />,
   [BootstrapIcon.STAR]: <StarIcon />,
+  [BootstrapIcon.PENCIL]: <PencilIcon />,
   [BootstrapIcon.TRASH]: <TrashIcon />,
   [BootstrapIcon.THREE_DOTS_HORIZONAL]: <ThreeDotsHorisonalIcon />,
   [BootstrapIcon.THREE_DOTS_VERTICAL]: <ThreeDotsVerticalIcon />,
