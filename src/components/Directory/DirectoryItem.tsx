0599d7775ee76f58dda1de017ea1cf78d1b1fe95
itizawa <itizawa@weseek.co.jp>
2021-04-28 00:19:36 +0900

    adjust layout

--- src/components/Directory/DirectoryItem.tsx
+++ src/components/Directory/DirectoryItem.tsx
@@ -18,12 +18,21 @@ export const DirectoryItem: VFC<Props> = ({ directory }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   const isActive = router.query.id != null && directory?._id === router.query.id;
 
+  const handleToggleCollapse = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setIsOpen((prevState) => !prevState);
+  };
+
+  const handleClickPencilIcon = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+  };
+
   return (
     <>
       <StyledDiv
-        className="text-white text-left w-100 rounded text-truncate"
+        className="text-white text-left w-100 rounded text-truncate d-flex"
         role="button"
-        onClick={() => setIsOpen((prevState) => !prevState)}
+        onClick={() => router.push(`/directory/${directory?._id}`)}
         isActive={isActive}
       >
         {isOpen ? (
@@ -34,6 +43,7 @@ export const DirectoryItem: VFC<Props> = ({ directory }: Props) => {
             icon={BootstrapIcon.CARET_DOWN}
             color={BootstrapColor.SECONDARY}
             activeColor={BootstrapColor.WHITE}
+            onClickButton={handleToggleCollapse}
             isRemovePadding
           />
         ) : (
@@ -44,12 +54,23 @@ export const DirectoryItem: VFC<Props> = ({ directory }: Props) => {
             icon={BootstrapIcon.CARET_RIGHT}
             color={BootstrapColor.SECONDARY}
             activeColor={BootstrapColor.WHITE}
+            onClickButton={handleToggleCollapse}
             isRemovePadding
           />
         )}
-        <Link href={`/directory/${directory?._id}`}>
-          <a className="text-decoration-none text-white">{directory?.name}</a>
-        </Link>
+        <span>{directory?.name}</span>
+        <div className="ms-auto pencil-button">
+          <IconButton
+            width={18}
+            height={18}
+            isActive={isActive}
+            icon={BootstrapIcon.PENCIL}
+            color={BootstrapColor.SECONDARY}
+            activeColor={BootstrapColor.WHITE}
+            onClickButton={handleClickPencilIcon}
+            isRemovePadding
+          />
+        </div>
       </StyledDiv>
       <Collapse isOpen={isOpen}>
         <div className="ps-3">{isOpen && <DirectoryItem />}</div>
@@ -59,14 +80,23 @@ export const DirectoryItem: VFC<Props> = ({ directory }: Props) => {
 };
 
 const StyledDiv = styled.div<{ isActive?: boolean }>`
+  .pencil-button {
+    display: none;
+  }
+
+  &:hover {
+    .pencil-button {
+      display: block;
+    }
+  }
   ${({ isActive }) =>
     isActive
       ? `
-    background-color: #00acc1;
+    background-color: #6f42c1;
     box-shadow: 0 12px 20px -10px rgba(0, 172, 193, 0.28), 0 4px 20px 0 rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(0, 172, 193, 0.2);
   `
       : `:hover {
-  background-color: rgba(200, 200, 200, 0.2);
-  transition: all 300ms linear;
+    background-color: rgba(200, 200, 200, 0.2);
+    transition: all 300ms linear;
   }`}
 `;
