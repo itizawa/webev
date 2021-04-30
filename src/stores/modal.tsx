a49457bf6df480db1c37dd727b1e3049a8472786
itizawa <itizawa@weseek.co.jp>
2021-04-30 22:42:47 +0900

    refactor Page

--- src/stores/modal.tsx
+++ src/stores/modal.tsx
@@ -1,6 +1,6 @@
 import { SWRResponse } from 'swr';
 import { Directory } from '~/interfaces/directory';
-import { Page } from '~/interfaces/page';
+import { Page } from '~/domains/Page';
 import { useStaticSWR } from '~/stores/use-static-swr';
 
 export const usePageForDelete = (initialData?: Page): SWRResponse<Page, Error> => {
