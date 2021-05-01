f8e6316c910f50ce4d3c1739d9cd1fd34379177c
itizawa <itizawa@weseek.co.jp>
2021-05-01 10:47:18 +0900

    display modal

--- src/components/organisms/SubnavBar.tsx
+++ src/components/organisms/SubnavBar.tsx
@@ -1,36 +1,62 @@
-import { VFC } from 'react';
+import { useState, VFC } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import style from 'styled-components';
+import styled from 'styled-components';
 
-import { BootstrapColor } from '~/interfaces/variables';
+import { Modal, ModalBody, ModalHeader } from 'reactstrap';
+import { IconButton } from '../Icons/IconButton';
+import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
 
 import { Icon } from '~/components/Icons/Icon';
 import { navbarItemMappings } from '~/const/navbarItemMappings';
+import { useLocale } from '~/hooks/useLocale';
 
 export const SubnavBar: VFC = () => {
   const router = useRouter();
-
+  const { t } = useLocale();
+  const [isDisplayDirectoryHierarchie, setIsDisplayDirectoryHierarchie] = useState(false);
+  const closeDirectoryHierarchieModal = () => {
+    setIsDisplayDirectoryHierarchie(false);
+  };
   return (
-    <StyledSubnavBar className="sticky-top bg-dark d-flex justify-content-evenly d-md-none">
-      {navbarItemMappings.map((v) => {
-        return (
-          <Link key={v.text} href={v.url}>
-            <StyledSubnavBarItem className="text-center col py-2" isActive={v.url === router.pathname}>
-              {v.icon != null && <Icon icon={v.icon} color={BootstrapColor.SECONDARY} />}
-              <span className="ms-1">{v.text}</span>
-            </StyledSubnavBarItem>
-          </Link>
-        );
-      })}
-    </StyledSubnavBar>
+    <>
+      <StyledSubnavBar className="sticky-top bg-dark d-flex justify-content-evenly d-md-none">
+        {navbarItemMappings.map((v) => {
+          return (
+            <Link key={v.text} href={v.url}>
+              <StyledSubnavBarItem className="text-center col py-2" isActive={v.url === router.pathname}>
+                {v.icon != null && <Icon icon={v.icon} color={BootstrapColor.SECONDARY} />}
+                <span className="ms-1">{v.text}</span>
+              </StyledSubnavBarItem>
+            </Link>
+          );
+        })}
+        <IconButton
+          width={24}
+          height={24}
+          icon={BootstrapIcon.DIRECTORY}
+          color={BootstrapColor.SECONDARY}
+          activeColor={BootstrapColor.SECONDARY}
+          isActive={isDisplayDirectoryHierarchie}
+          onClickButton={() => setIsDisplayDirectoryHierarchie(true)}
+        />
+      </StyledSubnavBar>
+      <Modal isOpen={isDisplayDirectoryHierarchie} toggle={closeDirectoryHierarchieModal}>
+        <ModalHeader className="bg-dark">{t.directory}</ModalHeader>
+        <ModalBody className="bg-dark text-break">
+          <button className="mx-auto btn btn-secondary w-100" onClick={closeDirectoryHierarchieModal}>
+            {t.cancel}
+          </button>
+        </ModalBody>
+      </Modal>
+    </>
   );
 };
 
-const StyledSubnavBar = style.div`
+const StyledSubnavBar = styled.div`
   top: -1px;
 `;
 
-const StyledSubnavBarItem = style.div<{ isActive: boolean }>`
+const StyledSubnavBarItem = styled.div<{ isActive: boolean }>`
   ${({ isActive }) => isActive && `border-bottom: 4px solid slateblue;`}
 `;
--- src/const/navbarItemMappings.ts
+++ src/const/navbarItemMappings.ts
@@ -3,5 +3,4 @@ import { BootstrapIcon } from '~/interfaces/variables';
 export const navbarItemMappings = [
   { text: 'Home', url: '/home', icon: BootstrapIcon.HOME },
   { text: 'Archive', url: '/archived', icon: BootstrapIcon.ARCHIVE },
-  { text: 'Directory', url: '/directory', icon: BootstrapIcon.DIRECTORY },
 ];
