c38213aa8bf307c7e61e2b9985fc2dad2330cf21
itizawa <itizawa@weseek.co.jp>
2021-05-01 10:51:38 +0900

    display DirectoryItem

--- src/components/organisms/SubnavBar.tsx
+++ src/components/organisms/SubnavBar.tsx
@@ -5,19 +5,32 @@ import styled from 'styled-components';
 
 import { Modal, ModalBody, ModalHeader } from 'reactstrap';
 import { IconButton } from '../Icons/IconButton';
+import { DirectoryItem } from '../Directory/DirectoryItem';
 import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
 
 import { Icon } from '~/components/Icons/Icon';
 import { navbarItemMappings } from '~/const/navbarItemMappings';
 import { useLocale } from '~/hooks/useLocale';
+import { useDirectoryListSWR } from '~/stores/directory';
 
 export const SubnavBar: VFC = () => {
-  const router = useRouter();
   const { t } = useLocale();
+  const router = useRouter();
+  const directoryId = router.query.id;
+
+  const { data: paginationResult } = useDirectoryListSWR();
+
   const [isDisplayDirectoryHierarchie, setIsDisplayDirectoryHierarchie] = useState(false);
+
   const closeDirectoryHierarchieModal = () => {
     setIsDisplayDirectoryHierarchie(false);
   };
+
+  const handleClickDirectory = (directoryId: string) => {
+    setIsDisplayDirectoryHierarchie(false);
+    router.push(`/directory/${directoryId}`);
+  };
+
   return (
     <>
       <StyledSubnavBar className="sticky-top bg-dark d-flex justify-content-evenly d-md-none">
@@ -44,7 +57,10 @@ export const SubnavBar: VFC = () => {
       <Modal isOpen={isDisplayDirectoryHierarchie} toggle={closeDirectoryHierarchieModal}>
         <ModalHeader className="bg-dark">{t.directory}</ModalHeader>
         <ModalBody className="bg-dark text-break">
-          <button className="mx-auto btn btn-secondary w-100" onClick={closeDirectoryHierarchieModal}>
+          {paginationResult?.docs.map((directory) => {
+            return <DirectoryItem key={directory._id} directory={directory} onClickDirectory={handleClickDirectory} activeDirectoryId={directoryId as string} />;
+          })}
+          <button className="mt-3 btn btn-secondary w-100" onClick={closeDirectoryHierarchieModal}>
             {t.cancel}
           </button>
         </ModalBody>
