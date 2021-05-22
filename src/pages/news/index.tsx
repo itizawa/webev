ab1fb30c3d01e3b50582c84ba768cf4540e0b20a
itizawa <itizawa@weseek.co.jp>
2021-05-22 20:32:11 +0900

    display contents

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,17 +1,38 @@
-import axios from 'axios';
+import Link from 'next/link';
 import { VFC } from 'react';
+import axios from 'axios';
 
 import { useLocale } from '~/hooks/useLocale';
 
-const Index: VFC = (props) => {
+type Props = {
+  data: {
+    contents: Array<{
+      id: string;
+      title: string;
+    }>;
+  };
+};
+
+const Index: VFC<Props> = (props: Props) => {
   const { t } = useLocale();
-  console.log(props);
+  const { contents } = props.data;
 
   return (
     <div className="p-3">
-      <div className="d-flex align-items-center">
+      <div className="d-flex align-items-center mb-3">
         <h1 className="mb-0">{t.news}</h1>
       </div>
+      <ul>
+        {contents.map((v) => {
+          return (
+            <li key={v.id}>
+              <Link href={`/news/${v.id}`}>
+                <a className="text-white fw-bold text-decoration-none">{v.title}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
