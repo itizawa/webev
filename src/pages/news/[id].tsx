fddc9a88c94f12ca012c9d9276b44e85e6b6b48c
itizawa <itizawa@weseek.co.jp>
2021-05-22 21:04:48 +0900

    display news page

---
+++ src/pages/news/[id].tsx
@@ -0,0 +1,75 @@
+import { VFC } from 'react';
+import axios from 'axios';
+
+type Props = {
+  news: {
+    id: string;
+    title: string;
+  };
+};
+
+const Index: VFC<Props> = (props: Props) => {
+  const { news } = props;
+  console.log(news);
+
+  return (
+    <div className="p-3">
+      <div className="d-flex align-items-center mb-3">
+        <h1 className="mb-0">{news.title}</h1>
+      </div>
+    </div>
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const getStaticPaths = async () => {
+  const key: { headers: { [key: string]: string } } = {
+    headers: { 'X-API-KEY': process.env.API_KEY as string },
+  };
+
+  try {
+    const response: { data: { contents: Array<{ id: string }> } } = await axios.get('https://webev.microcms.io/api/v1/news', key);
+    const { data } = response;
+
+    const paths = data.contents.map((content) => `/news/${content.id}`);
+    return { paths, fallback: false };
+  } catch (error) {
+    console.log(error);
+    return { paths: [], fallback: false };
+  }
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const getStaticProps = async (context: { params: { id: string } }) => {
+  const id = context.params.id || null;
+  if (id == null) {
+    return {
+      props: {
+        news: null,
+      },
+    };
+  }
+
+  const key: { headers: { [key: string]: string } } = {
+    headers: { 'X-API-KEY': process.env.API_KEY as string },
+  };
+  try {
+    const response = await axios.get(`https://webev.microcms.io/api/v1/news/${id}`, key);
+    const { data } = response;
+
+    return {
+      props: {
+        news: data,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: {
+        news: null,
+      },
+    };
+  }
+};
+
+export default Index;
