73c5004c24ca4a71e10173049be78a4c8013844f
itizawa <itizawa@weseek.co.jp>
2021-05-22 20:24:29 +0900

    implement request

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,9 +1,11 @@
+import axios from 'axios';
 import { VFC } from 'react';
 
 import { useLocale } from '~/hooks/useLocale';
 
-const Index: VFC = () => {
+const Index: VFC = (props) => {
   const { t } = useLocale();
+  console.log(props);
 
   return (
     <div className="p-3">
@@ -12,6 +14,28 @@ const Index: VFC = () => {
       </div>
     </div>
   );
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const getStaticProps = async () => {
+  const key: { headers: { [key: string]: string } } = {
+    headers: { 'X-API-KEY': process.env.API_KEY as string },
+  };
+  try {
+    const response = await axios.get('https://webev.microcms.io/api/v1/news', key);
+    const { data } = response;
+
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: {},
+    };
+  }
 };
 
 export default Index;
