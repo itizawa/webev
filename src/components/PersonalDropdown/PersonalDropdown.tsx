5965febc558fa8306b106aa31e31a00b4bb33779
itizawa <itizawa@weseek.co.jp>
2021-05-29 17:28:54 +0900

    implement change layout type

--- src/components/PersonalDropdown/PersonalDropdown.tsx
+++ src/components/PersonalDropdown/PersonalDropdown.tsx
@@ -10,6 +10,9 @@ import { UserIcon } from '~/components/Icons/UserIcon';
 
 import { useLocale } from '~/hooks/useLocale';
 
+import { useOgpCardLayout } from '~/stores/contexts';
+import { OgpLayoutType } from '~/interfaces/contexts';
+
 import { User } from '~/interfaces/user';
 import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
 
@@ -20,6 +23,8 @@ type Props = {
 export const PersonalDropdown: VFC<Props> = (props: Props) => {
   const { t } = useLocale();
   const { user } = props;
+
+  const { data: ogpCardLayout, mutate: mutateOgpCardLayout } = useOgpCardLayout();
 
   const [isEnableReadFromClipboard, setIsEnableReadFromClipboard] = useState(false);
 
@@ -33,6 +38,10 @@ export const PersonalDropdown: VFC<Props> = (props: Props) => {
     setIsEnableReadFromClipboard(bool);
     localStorage.setItem('isEnableReadFromClipboard', bool.toString());
     toastSuccess(t.toastr_update_setting);
+  };
+
+  const handleClickOgpCardLayout = (type: OgpLayoutType) => {
+    mutateOgpCardLayout(type);
   };
 
   return (
@@ -62,10 +71,16 @@ export const PersonalDropdown: VFC<Props> = (props: Props) => {
           </div>
         </div>
         <div className="px-3 my-3 d-flex justify-content-between">
-          <button className="btn btn-outline-indigo active">
+          <button
+            className={`btn btn-outline-indigo ${ogpCardLayout === OgpLayoutType.LIST ? 'active' : ''}`}
+            onClick={() => handleClickOgpCardLayout(OgpLayoutType.LIST)}
+          >
             <Icon height={20} width={20} icon={BootstrapIcon.LIST} color={BootstrapColor.WHITE} />
           </button>
-          <button className="btn btn-outline-indigo active">
+          <button
+            className={`btn btn-outline-indigo ${ogpCardLayout === OgpLayoutType.CARD ? 'active' : ''}`}
+            onClick={() => handleClickOgpCardLayout(OgpLayoutType.CARD)}
+          >
             <Icon height={20} width={20} icon={BootstrapIcon.GRID} color={BootstrapColor.WHITE} />
           </button>
         </div>
