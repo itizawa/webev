374ed6e96f16dd71a8d58e37e2c3a6f1efab8c0e
itizawa <itizawa@weseek.co.jp>
2021-05-29 16:00:15 +0900

    implement list icon

--- src/interfaces/variables.ts
+++ src/interfaces/variables.ts
@@ -15,6 +15,7 @@ export enum BootstrapIcon {
   PLUS_BOARD = 'plus',
   PLUS_DOTTED = 'plus-dotted',
   CHECK = 'check',
+  LIST = 'list',
   OUTLINE_CHECK = 'outline-check',
   REPLY = 'reply',
   STAR = 'star',
