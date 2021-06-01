ab7794fd9e52a128bc739a072a2c7fca2a521601
itizawa <itizawa@weseek.co.jp>
2021-06-02 00:41:25 +0900

    use useSearchKeyWord

--- src/components/Commons/SearchForm.tsx
+++ src/components/Commons/SearchForm.tsx
@@ -1,21 +1,30 @@
-import { useState, VFC } from 'react';
+import { useEffect, useState, VFC } from 'react';
 import { Icon } from '../Icons/Icon';
 import { useLocale } from '~/hooks/useLocale';
 import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
+import { useSearchKeyWord } from '~/stores/page';
 
 export const SearchForm: VFC = () => {
   const [inputValue, setInputValue] = useState('');
 
   const { t } = useLocale();
+  const { data: searchKeyWord, mutate: mutateSearchKeyord } = useSearchKeyWord();
 
-  const onSubmitSearchForm = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+  useEffect(() => {
+    if (searchKeyWord != null) {
+      setInputValue(searchKeyWord);
+    }
+  }, [searchKeyWord]);
+
+  const onSubmitSearchForm = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    console.log('hoge');
+
+    mutateSearchKeyord(inputValue);
   };
 
   return (
     <form className="input-group input-group-sm" onSubmit={onSubmitSearchForm}>
-      <button className="btn btn-secondary text-white" type="submit" id="input-group" disabled={inputValue.length === 0}>
+      <button className="btn btn-secondary text-white" type="submit" id="input-group-search" disabled={inputValue.length === 0}>
         <Icon height={16} width={16} icon={BootstrapIcon.SEARCH} color={BootstrapColor.WHITE} />
         <span className="ms-2">{t.search}</span>
       </button>
