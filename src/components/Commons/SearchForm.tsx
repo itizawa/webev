3a74bc95bd40553f9fdbb4f9c99e80cc292cea93
itizawa <itizawa@weseek.co.jp>
2021-06-02 00:29:09 +0900

    display search icon

--- src/components/Commons/SearchForm.tsx
+++ src/components/Commons/SearchForm.tsx
@@ -1,5 +1,7 @@
 import { useState, VFC } from 'react';
+import { Icon } from '../Icons/Icon';
 import { useLocale } from '~/hooks/useLocale';
+import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
 
 export const SearchForm: VFC = () => {
   const [inputValue, setInputValue] = useState('');
@@ -13,10 +15,11 @@ export const SearchForm: VFC = () => {
 
   return (
     <form className="input-group input-group-sm" onSubmit={onSubmitSearchForm}>
-      <button className="btn btn-secondary" type="submit" id="input-group" disabled={inputValue.length === 0}>
-        {t.search}
+      <button className="btn btn-secondary text-white" type="submit" id="input-group" disabled={inputValue.length === 0}>
+        <Icon height={16} width={16} icon={BootstrapIcon.SEARCH} color={BootstrapColor.WHITE} />
+        <span className="ms-2">{t.search}</span>
       </button>
-      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} className="form-control ps-3 bg-white" />
+      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} className="form-control bg-white" />
     </form>
   );
 };
