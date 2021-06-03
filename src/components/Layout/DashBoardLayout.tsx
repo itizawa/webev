c27a1798cff7a52fd20bbe6ead0f3e507abebac0
itizawa <itizawa@weseek.co.jp>
2021-06-03 23:51:47 +0900

    fix lint err

--- src/components/Layout/DashBoardLayout.tsx
+++ src/components/Layout/DashBoardLayout.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { FC, useEffect } from 'react';
 import { useSession } from 'next-auth/client';
 import { useRouter } from 'next/router';
--- src/components/Layout/DefaultLayout.tsx
+++ src/components/Layout/DefaultLayout.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { FC } from 'react';
 import styled from 'styled-components';
 
