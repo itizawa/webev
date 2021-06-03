643a43008533bf5442af3dd7b86908a6387e91a9
itizawa <itizawa@weseek.co.jp>
2021-06-03 23:43:18 +0900

    add title login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,6 @@
-import { useState, VFC } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
+import { useState, VFC } from 'react';
 
 import { signIn } from 'next-auth/client';
 import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
@@ -13,23 +14,28 @@ const Index: VFC = () => {
   const [isCheckedAgree, setIsCheckedAgree] = useState(false);
 
   return (
-    <LoginRequiredWrapper>
-      <div className="p-3">
-        <h1>Login Page</h1>
-        {router.query.isRedirect && <div className="alert alert-warning">ログインが必要です</div>}
-        <Term />
-        <div className="form-check form-check-inline mb-3">
-          <input className="form-check-input" type="checkbox" id="termAgreement" checked={isCheckedAgree} onChange={() => setIsCheckedAgree(!isCheckedAgree)} />
-          <label className="form-check-label" htmlFor="termAgreement">
-            利用規約に同意する
-          </label>
+    <>
+      <Head>
+        <title>Webev | Login</title>
+      </Head>
+      <LoginRequiredWrapper>
+        <div className="p-3">
+          <h1>Login Page</h1>
+          {router.query.isRedirect && <div className="alert alert-warning">ログインが必要です</div>}
+          <Term />
+          <div className="form-check form-check-inline mb-3">
+            <input className="form-check-input" type="checkbox" id="termAgreement" checked={isCheckedAgree} onChange={() => setIsCheckedAgree(!isCheckedAgree)} />
+            <label className="form-check-label" htmlFor="termAgreement">
+              利用規約に同意する
+            </label>
+          </div>
+          <button type="button" className="btn btn-google w-100" onClick={() => signIn('google')} disabled={!isCheckedAgree}>
+            <Icon icon={BootstrapIcon.GOOGLE} color={BootstrapColor.LIGHT} />
+            <span className="ms-2">Google でログインする</span>
+          </button>
         </div>
-        <button type="button" className="btn btn-google w-100" onClick={() => signIn('google')} disabled={!isCheckedAgree}>
-          <Icon icon={BootstrapIcon.GOOGLE} color={BootstrapColor.LIGHT} />
-          <span className="ms-2">Google でログインする</span>
-        </button>
-      </div>
-    </LoginRequiredWrapper>
+      </LoginRequiredWrapper>
+    </>
   );
 };
 
