c4e3c47e1e05f4c2bcafb6bca1d20503078932fe
itizawa <itizawa@weseek.co.jp>
2021-06-05 13:10:15 +0900

    implement email

--- src/pages/inquiry/index.tsx
+++ src/pages/inquiry/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import { VFC } from 'react';
 
+import styled from 'styled-components';
+
 import { useLocale } from '~/hooks/useLocale';
 
 const Index: VFC = () => {
@@ -12,10 +14,26 @@ const Index: VFC = () => {
         <title>Webev | {t.inquiry}</title>
       </Head>
       <div className="p-3">
-        <h1>{t.inquiry}</h1>
+        <h1 className="text-center">{t.inquiry}</h1>
+        <StyledDiv className="mx-auto">
+          <form className="mt-5">
+            <div className="mb-3 row">
+              <label htmlFor="inputEmail" className="col-sm-2 col-form-label">
+                Email
+              </label>
+              <div className="col-sm-10">
+                <input type="email" className="form-control" id="inputEmail" />
+              </div>
+            </div>
+          </form>
+        </StyledDiv>
       </div>
     </>
   );
 };
+
+const StyledDiv = styled.div`
+  max-width: 800px;
+`;
 
 export default Index;
