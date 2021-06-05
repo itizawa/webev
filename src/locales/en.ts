0a3dd77b3014474c4702fff47eebd1b8904953c8
itizawa <itizawa@weseek.co.jp>
2021-06-05 14:02:24 +0900

    implement inquiry type form

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -68,6 +68,7 @@ export const en = {
 
   inquiry_email_desc: 'Please enter if you need a reply',
   inquiry_type: 'Inquiry Type',
+  inquiry_email: 'Mail Address',
   open_select: 'Open this select menu',
   bug_report: 'Bug Request',
   violation_report: 'Violation Request',
--- src/pages/inquiry/index.tsx
+++ src/pages/inquiry/index.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head';
-import { VFC } from 'react';
+import { useState, VFC } from 'react';
 
 import styled from 'styled-components';
 
 import { useLocale } from '~/hooks/useLocale';
+import { InquiryType } from '~/domains/Inquiry';
 
 const Index: VFC = () => {
   const { t } = useLocale();
+
+  const [inquiryType, setInquiryType] = useState<InquiryType>();
+  const [inquiryEmail, setInquiryEmail] = useState<string>();
 
   return (
     <>
@@ -16,13 +20,28 @@ const Index: VFC = () => {
       <div className="p-3">
         <h1 className="text-center">{t.inquiry}</h1>
         <StyledDiv className="mx-auto">
-          <form className="mt-5">
-            <div className="mb-3 row">
-              <label htmlFor="inputEmail" className="col-sm-2 col-form-label">
-                Email
+          <form className="mt-3 mt-lg-5">
+            <div className="mb-3 mb-lg-4">
+              <label className="col-form-label">{t.inquiry_type}</label>
+              <div>
+                <select className="form-select" onChange={(e) => setInquiryType(e.target.value as InquiryType)}>
+                  <option selected={inquiryType == null}>{t.open_select}</option>
+                  {Object.values(InquiryType).map((v) => {
+                    return (
+                      <option key={v} selected={inquiryType == v} value={v}>
+                        {t[v]}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+            </div>
+            <div className="mb-3 mb-lg-4">
+              <label htmlFor="inputEmail" placeholder="hoge@example.com" className="col-form-label">
+                {t.inquiry_email}
               </label>
-              <div className="col-sm-10">
-                <input type="email" className="form-control" id="inputEmail" />
+              <div>
+                <input type="email" value={inquiryEmail} className="form-control" id="inputEmail" onChange={(e) => setInquiryEmail(e.target.value)} />
               </div>
             </div>
           </form>
@@ -33,7 +52,7 @@ const Index: VFC = () => {
 };
 
 const StyledDiv = styled.div`
-  max-width: 800px;
+  max-width: 600px;
 `;
 
 export default Index;
