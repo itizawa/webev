f753c2a654102f661fecbd47f090caf67fb31838
itizawa <itizawa@weseek.co.jp>
2021-06-05 22:45:46 +0900

    adjust head

--- src/components/Commons/WebevOgpHead.tsx
+++ src/components/Commons/WebevOgpHead.tsx
@@ -1,4 +1,4 @@
-import { Head } from 'next/document';
+import Head from 'next/head';
 import { VFC } from 'react';
 import { imagePath } from '~/const/imagePath';
 
@@ -16,17 +16,19 @@ export const WebevOgpHead: VFC<Props> = (props: Props) => {
   const { siteName, title, url, image, description } = props;
 
   return (
-    <Head>
-      <title>{title == null ? 'Webev' : `Webev - ${title}`}</title>
-      <meta property="og:site_name" content={siteName || 'Webev'} />
-      <meta property="og:title" content={title || 'Webev'} />
-      <meta property="og:url" content={url || 'https://www.webev.cloud'} />
-      <meta property="og:image" content={image || imagePath.EYE_CATCH_DARK} />
-      <meta property="og:description" content={description || DESCRIPTION} />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:title" content={title || 'Webev'} />
-      <meta property="twitter:image" content={image || imagePath.EYE_CATCH_DARK} />
-      <meta name="twitter:description" content={description || DESCRIPTION} />
-    </Head>
+    <>
+      <Head>
+        <title>{title || 'Webev'}</title>
+        <meta property="og:site_name" content={siteName || 'Webev'} />
+        <meta property="og:title" content={title || 'Webev'} />
+        <meta property="og:url" content={url || 'https://www.webev.cloud'} />
+        <meta property="og:image" content={image || imagePath.EYE_CATCH_DARK} />
+        <meta property="og:description" content={description || DESCRIPTION} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={title || 'Webev'} />
+        <meta property="twitter:image" content={image || imagePath.EYE_CATCH_DARK} />
+        <meta name="twitter:description" content={description || DESCRIPTION} />
+      </Head>
+    </>
   );
 };
