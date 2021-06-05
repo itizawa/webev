e6100e42b885084f54573c30eb89687538e1a1fe
itizawa <itizawa@weseek.co.jp>
2021-06-06 01:29:35 +0900

    adjust i18n

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -42,7 +42,7 @@ export const en = {
 
   return_news_list: 'Return News List',
 
-  read_button: 'Read',
+  read_button: 'Mark as Read',
   return_button: 'Put Back Home',
 
   child_directory: 'Child Directories',
