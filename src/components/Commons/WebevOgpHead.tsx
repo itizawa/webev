08c4891df1a0e03953f7ee244c22be51121ff24e
itizawa <itizawa@weseek.co.jp>
2021-06-05 22:37:18 +0900

    adjust twitter

--- src/components/Commons/WebevOgpHead.tsx
+++ src/components/Commons/WebevOgpHead.tsx
@@ -1,22 +1,32 @@
 import { Head } from 'next/document';
 import { VFC } from 'react';
+import { imagePath } from '~/const/imagePath';
+
 type Props = {
   siteName?: string;
   title?: string;
   url?: string;
+  image?: string;
   description?: string;
 };
+
+const DESCRIPTION = 'Webev は、誰でも使えるブックマークマネージャーです！URL を入力して保存するだけで Web ページを管理できます。';
+
 export const WebevOgpHead: VFC<Props> = (props: Props) => {
-  const { siteName, title, url, description } = props;
+  const { siteName, title, url, image, description } = props;
+
   return (
     <Head>
+      <title>Webev - {title}</title>
       <meta property="og:site_name" content={siteName || 'Webev'} />
       <meta property="og:title" content={title || 'Webev'} />
       <meta property="og:url" content={url || 'https://www.webev.cloud'} />
-      <meta
-        property="og:description"
-        content={description || 'Webev は、誰でも使えるブックマークマネージャーです！URL を入力して保存するだけで Web ページを管理できます。'}
-      />
+      <meta property="og:image" content={image || imagePath.EYE_CATCH_DARK} />
+      <meta property="og:description" content={description || DESCRIPTION} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title || 'Webev'} />
+      <meta property="twitter:image" content={image || imagePath.EYE_CATCH_DARK} />
+      <meta name="twitter:description" content={description || DESCRIPTION} />
     </Head>
   );
 };
