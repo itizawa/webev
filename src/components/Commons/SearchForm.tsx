d69f3474ba04afe0b9b13f5a366ea66f25ff61d5
itizawa <itizawa@weseek.co.jp>
2021-06-06 23:58:38 +0900

    expand on focus

--- src/components/Commons/SearchForm.tsx
+++ src/components/Commons/SearchForm.tsx
@@ -1,4 +1,7 @@
 import { useEffect, useState, VFC } from 'react';
+
+import styled from 'styled-components';
+
 import { Icon } from '../Icons/Icon';
 import { useLocale } from '~/hooks/useLocale';
 import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
@@ -6,6 +9,7 @@ import { useSearchKeyWord } from '~/stores/page';
 
 export const SearchForm: VFC = () => {
   const [inputValue, setInputValue] = useState('');
+  const [isExpand, setIsExpand] = useState(false);
 
   const { t } = useLocale();
   const { data: searchKeyWord, mutate: mutateSearchKeyord } = useSearchKeyWord();
@@ -23,12 +27,23 @@ export const SearchForm: VFC = () => {
   };
 
   return (
-    <form className="input-group input-group-sm" onSubmit={onSubmitSearchForm}>
+    <StyledForm
+      isExpand={isExpand}
+      className="input-group input-group-sm"
+      onSubmit={onSubmitSearchForm}
+      onFocus={() => setIsExpand(true)}
+      onBlur={() => setIsExpand(false)}
+    >
       <button className="btn btn-secondary text-white" type="submit" id="input-group-search">
         <Icon height={16} width={16} icon={BootstrapIcon.SEARCH} color={BootstrapColor.WHITE} />
         <span className="ms-2">{t.search}</span>
       </button>
       <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} className="form-control bg-white" />
-    </form>
+    </StyledForm>
   );
 };
+
+const StyledForm = styled.form<{ isExpand: boolean }>`
+  ${({ isExpand }) => isExpand && `width: 400px;`}
+  transition: all 1000ms;
+`;
