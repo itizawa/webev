ecdbabbe8bf223a5d26af4da9e144f9c984eabe6
itizawa <itizawa@weseek.co.jp>
2021-06-10 23:36:41 +0900

    implement save handler

--- src/components/ShareLinkReceiver.tsx
+++ src/components/ShareLinkReceiver.tsx
@@ -2,13 +2,21 @@ import { useRouter } from 'next/router';
 import { useEffect, useState, VFC } from 'react';
 
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+
 import { useLocale } from '~/hooks/useLocale';
+import { useSocketId } from '~/stores/contexts';
+import { usePageListSWR } from '~/stores/page';
+import { restClient } from '~/utils/rest-client';
+import { toastError, toastSuccess } from '~/utils/toastr';
 
 type Props = {};
 
 export const ShareLinkReceiver: VFC<Props> = () => {
   const router = useRouter();
   const { t } = useLocale();
+
+  const { data: socketId } = useSocketId();
+  const { mutate: mutatePageList } = usePageListSWR();
 
   const [title, setTitle] = useState<string | null>();
   const [url, setUrl] = useState<string | null>();
@@ -18,22 +26,34 @@ export const ShareLinkReceiver: VFC<Props> = () => {
     setUrl(router.query.url as string);
   }, [router]);
 
-  const handleCloseButton = () => {
+  const handleClickCloseButton = () => {
     setTitle(null);
     setUrl(null);
   };
 
+  const handleClickSubmitButton = async () => {
+    try {
+      await restClient.apiPost('/pages', { url, socketId });
+      toastSuccess(t.toastr_save_url);
+      setTitle(null);
+      setUrl(null);
+      mutatePageList();
+    } catch (err) {
+      toastError(err);
+    }
+  };
+
   return (
-    <Modal isOpen={title != null && url != null} toggle={handleCloseButton}>
+    <Modal isOpen={title != null && url != null} toggle={handleClickCloseButton}>
       <ModalHeader className="bg-dark">{t.save_page}</ModalHeader>
       <ModalBody className="bg-dark text-break text-center">
         <h5 className="text-center my-3">{title}</h5>
         <h5 className="">{url}</h5>
         <div className="d-flex justify-content-evenly mt-5">
-          <button className="btn btn-secondary" onClick={handleCloseButton}>
+          <button className="btn btn-secondary" onClick={handleClickCloseButton}>
             {t.cancel}
           </button>
-          <button className="btn btn-danger" onClick={handleCloseButton}>
+          <button className="btn btn-indigo" onClick={handleClickSubmitButton}>
             {t.save}
           </button>
         </div>
