031e7250169f2ae5dea076428c50704e9dd87a02
itizawa <itizawa@weseek.co.jp>
2021-06-10 23:22:08 +0900

    create ShareLinkReceiver

---
+++ src/components/ShareLinkReceiver.tsx
@@ -0,0 +1,17 @@
+import { VFC } from 'react';
+
+type Props = {};
+
+export const ShareLinkReceiver: VFC<Props> = () => {
+  // useEffect(() => {
+  //   mutatePageStatus(PathConfigs[pathname].statusForFind);
+  //   mutateSearchKeyord('');
+
+  //   if (router.pathname !== '/directory/[id]') {
+  //     mutateDirectoryId(null);
+  //   }
+  //   mutateActivePage(1);
+  // }, [router]);
+
+  return <div>hoge</div>;
+};
