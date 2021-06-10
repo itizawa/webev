fe3bc2cc0091da574d3bf6892ea749049471f4a5
itizawa <itizawa@weseek.co.jp>
2021-06-10 23:31:10 +0900

    display modal

--- src/components/ShareLinkReceiver.tsx
+++ src/components/ShareLinkReceiver.tsx
@@ -1,17 +1,43 @@
-import { VFC } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState, VFC } from 'react';
+
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { useLocale } from '~/hooks/useLocale';
 
 type Props = {};
 
 export const ShareLinkReceiver: VFC<Props> = () => {
-  // useEffect(() => {
-  //   mutatePageStatus(PathConfigs[pathname].statusForFind);
-  //   mutateSearchKeyord('');
+  const router = useRouter();
+  const { t } = useLocale();
 
-  //   if (router.pathname !== '/directory/[id]') {
-  //     mutateDirectoryId(null);
-  //   }
-  //   mutateActivePage(1);
-  // }, [router]);
+  const [title, setTitle] = useState<string | null>();
+  const [url, setUrl] = useState<string | null>();
 
-  return <div>hoge</div>;
+  useEffect(() => {
+    setTitle(router.query.title as string);
+    setUrl(router.query.url as string);
+  }, [router]);
+
+  const handleCloseButton = () => {
+    setTitle(null);
+    setUrl(null);
+  };
+
+  return (
+    <Modal isOpen={title != null && url != null} toggle={handleCloseButton}>
+      <ModalHeader className="bg-dark">{t.save_page}</ModalHeader>
+      <ModalBody className="bg-dark text-break text-center">
+        <h5 className="text-center my-3">{title}</h5>
+        <h5 className="">{url}</h5>
+        <div className="d-flex justify-content-evenly mt-5">
+          <button className="btn btn-secondary" onClick={handleCloseButton}>
+            {t.cancel}
+          </button>
+          <button className="btn btn-danger" onClick={handleCloseButton}>
+            {t.save}
+          </button>
+        </div>
+      </ModalBody>
+    </Modal>
+  );
 };
