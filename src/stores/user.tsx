79236e4cbafab4f293b18947cb2d550f48df9368
itizawa <itizawa@weseek.co.jp>
2021-06-12 17:42:03 +0900

    rename

--- src/stores/user.tsx
+++ src/stores/user.tsx
@@ -4,7 +4,7 @@ import { restClient } from '~/utils/rest-client';
 import { User } from '~/domains/User';
 import { useAuthenticationSWR } from '~/stores/use-authentication-swr';
 
-export const useUserInfo = (): SWRResponse<User, Error> => {
+export const useCurrentUser = (): SWRResponse<User, Error> => {
   return useAuthenticationSWR('/users/me', (endpoint) => restClient.apiGet(endpoint).then((result) => result.data), {
     revalidateOnFocus: false,
     revalidateOnReconnect: true,
