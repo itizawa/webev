250a543eef4afccd0a5a3496021a3c8669bfb7a0
itizawa <itizawa@weseek.co.jp>
2021-06-20 18:48:17 +0900

    remove statusForFind

--- src/interfaces/route.ts
+++ src/interfaces/route.ts
@@ -24,46 +24,35 @@ export type LayoutName = typeof LayoutNames[keyof typeof LayoutNames];
 export const PathConfigs = {
   [PathNames.ROOT]: {
     layout: LayoutNames.DEFAULT,
-    statusForFind: [PageStatus.PAGE_STATUS_STOCK],
   },
   [PathNames.NOT_FOUND]: {
     layout: LayoutNames.DEFAULT,
-    statusForFind: [PageStatus.PAGE_STATUS_STOCK],
   },
   [PathNames.LOGIN]: {
     layout: LayoutNames.DEFAULT,
-    statusForFind: [PageStatus.PAGE_STATUS_STOCK],
   },
   [PathNames.TERM]: {
     layout: LayoutNames.DEFAULT,
-    statusForFind: [PageStatus.PAGE_STATUS_STOCK],
   },
   [PathNames.HOME]: {
     layout: LayoutNames.DASHBOARD,
-    statusForFind: [PageStatus.PAGE_STATUS_STOCK],
   },
   [PathNames.USER]: {
     layout: LayoutNames.DASHBOARD,
-    statusForFind: [PageStatus.PAGE_STATUS_STOCK],
   },
   [PathNames.READ]: {
     layout: LayoutNames.DASHBOARD,
-    statusForFind: [PageStatus.PAGE_STATUS_ARCHIVE],
   },
   [PathNames.DIRECTORY]: {
     layout: LayoutNames.DASHBOARD,
-    statusForFind: [PageStatus.PAGE_STATUS_STOCK, PageStatus.PAGE_STATUS_ARCHIVE],
   },
   [PathNames.DIRECTORY_ID]: {
     layout: LayoutNames.DASHBOARD,
-    statusForFind: [PageStatus.PAGE_STATUS_STOCK, PageStatus.PAGE_STATUS_ARCHIVE],
   },
   [PathNames.NEWS]: {
     layout: LayoutNames.DEFAULT,
-    statusForFind: [PageStatus.PAGE_STATUS_STOCK],
   },
   [PathNames.NEWS_ID]: {
     layout: LayoutNames.DEFAULT,
-    statusForFind: [PageStatus.PAGE_STATUS_STOCK],
   },
 };
