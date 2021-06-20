2d339c998e5d06dda10862379bbd73a017c99d61
itizawa <itizawa@weseek.co.jp>
2021-06-20 19:14:05 +0900

    implement Editable Input

---
+++ src/components/Atoms/EditableInput.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState, VFC } from 'react';
+
+import styled from 'styled-components';
+
+type Props = {
+  value: string;
+};
+
+export const EditableInput: VFC<Props> = (props) => {
+  const { value } = props;
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    setInputValue(value);
+  }, [value]);
+
+  return (
+    <StyledInput
+      className="form-control text-nowrap overflow-scroll fs-1 pt-0 pb-2 pb-md-0 me-auto w-100"
+      onChange={(e) => setInputValue(e.target.value)}
+      // onBlur={handleBlurTextInput}
+      value={inputValue || ''}
+    />
+  );
+};
+
+const StyledInput = styled.input`
+  color: #ccc;
+  background: transparent;
+  border: none;
+
+  &:hover {
+    color: #ccc;
+    background: #232323;
+    ::placeholder {
+      color: #ccc;
+    }
+  }
+
+  &:focus {
+    color: #ccc;
+    background: transparent;
+    ::placeholder {
+      color: #ccc;
+    }
+  }
+`;
