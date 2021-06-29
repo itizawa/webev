8d4ae34a0186a70d837d0322f1bf64af01f8fa03
kaori <kaori@weseek.co.jp>
2021-06-29 10:58:01 +0900

    fixed picker position temporarily

--- src/pages/directory/[id].tsx
+++ src/pages/directory/[id].tsx
@@ -217,8 +217,8 @@ const Index: VFC = () => {
                 </UncontrolledDropdown>
               </div>
               {isEmojiSettingMode && (
-                <StyledEmojiPicker className="bg-info position-fixed top-0 start-0 end-0 bottom-0" onClick={() => setIsEmojiSettingMode(false)}>
-                  <Picker theme="dark" onSelect={(emoji) => handleEmoji(emoji)} />
+                <StyledEmojiPicker className=" position-fixed top-0 start-0 end-0 bottom-0" onClick={() => setIsEmojiSettingMode(false)}>
+                  <Picker theme="dark" onSelect={(emoji) => handleEmoji(emoji)} style={{ position: 'absolute', left: '320px', top: '170px' }} />
                 </StyledEmojiPicker>
               )}
             </>
