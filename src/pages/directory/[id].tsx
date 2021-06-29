b3d25cf70509b3ebae1a1337dd9c594c40d5bbc6
itizawa <48426654+itizawa@users.noreply.github.com>
2021-06-29 10:48:33 +0900

    Update [id].tsx

--- src/pages/directory/[id].tsx
+++ src/pages/directory/[id].tsx
@@ -217,7 +217,7 @@ const Index: VFC = () => {
                 </UncontrolledDropdown>
               </div>
               {isEmojiSettingMode && (
-                <StyledEmojiPicker className="bg-info position-fixed top-0 start-0 end-0 bottom-0" onClick={() => setIsEmojiSettingMode(false)}>
+                <StyledEmojiPicker className="position-fixed top-0 start-0 end-0 bottom-0" onClick={() => setIsEmojiSettingMode(false)}>
                   <Picker theme="dark" onSelect={(emoji) => handleEmoji(emoji)} />
                 </StyledEmojiPicker>
               )}
