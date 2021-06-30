ae1773a8531852562bcb409b9b908fe23c58c7e1
kaori <kaori@weseek.co.jp>
2021-06-30 23:58:39 +0900

    rename functions

--- src/pages/directory/[id].tsx
+++ src/pages/directory/[id].tsx
@@ -146,7 +146,7 @@ const Index: VFC = () => {
     }
   };
 
-  const handleClickedEmoji = () => {
+  const handleClickEmoji = () => {
     setIsEmojiSettingMode(true);
     if (emojiRef.current != null) {
       setPiclerTop(emojiRef.current.offsetTop + emojiSize + 10);
@@ -183,7 +183,7 @@ const Index: VFC = () => {
               </div>
               <div className="d-flex gap-3 align-items-center mt-2">
                 <div ref={emojiRef}>
-                  <Emoji emoji={emoji} size={emojiSize} onClick={() => handleClickedEmoji()} />
+                  <Emoji emoji={emoji} size={emojiSize} onClick={() => handleClickEmoji()} />
                 </div>
                 <StyledInput
                   className="form-control text-nowrap overflow-scroll fs-1 pt-0 pb-2 pb-md-0 me-auto w-100"
