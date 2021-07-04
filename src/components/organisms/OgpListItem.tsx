f992c8ca316fe34187ed934d2e9fad3210f200c9
itizawa <itizawa@weseek.co.jp>
2021-07-04 13:52:54 +0900

    refactor component

--- src/components/organisms/OgpListItem.tsx
+++ src/components/organisms/OgpListItem.tsx
@@ -25,9 +25,10 @@ const MAX_WORD_COUNT_OF_SITENAME = 10;
 
 type Props = {
   page: Page;
+  isHideArchiveButton?: boolean;
 };
 
-export const OgpListItem: VFC<Props> = ({ page }) => {
+export const OgpListItem: VFC<Props> = ({ page, isHideArchiveButton }) => {
   const { t } = useLocale();
 
   const { mutate: mutatePageList } = usePageListSWR();
@@ -160,20 +161,22 @@ export const OgpListItem: VFC<Props> = ({ page }) => {
             </UncontrolledTooltip>
           )}
         </small>
-        <StyledButton className="btn btn-sm d-flex ms-auto" onClick={switchArchive}>
-          {status === PageStatus.PAGE_STATUS_ARCHIVE && (
-            <>
-              <Icon height={20} width={20} icon={BootstrapIcon.REPLY} color={BootstrapColor.WHITE} />
-              <span className="ms-2 text-nowrap">{t.return_button}</span>
-            </>
-          )}
-          {status === PageStatus.PAGE_STATUS_STOCK && (
-            <>
-              <Icon height={20} width={20} icon={BootstrapIcon.CHECK} color={BootstrapColor.WHITE} />
-              <span className="ms-2 text-nowrap">{t.read_button}</span>
-            </>
-          )}
-        </StyledButton>
+        {!isHideArchiveButton && (
+          <StyledButton className="btn btn-sm d-flex ms-auto" onClick={switchArchive}>
+            {status === PageStatus.PAGE_STATUS_ARCHIVE && (
+              <>
+                <Icon height={20} width={20} icon={BootstrapIcon.REPLY} color={BootstrapColor.WHITE} />
+                <span className="ms-2 text-nowrap">{t.return_button}</span>
+              </>
+            )}
+            {status === PageStatus.PAGE_STATUS_STOCK && (
+              <>
+                <Icon height={20} width={20} icon={BootstrapIcon.CHECK} color={BootstrapColor.WHITE} />
+                <span className="ms-2 text-nowrap">{t.read_button}</span>
+              </>
+            )}
+          </StyledButton>
+        )}
       </div>
     </StyledRow>
   );
