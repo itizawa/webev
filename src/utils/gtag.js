0f16da6642df452b1270392c85b4f0931966e932
itizawa <itizawa@weseek.co.jp>
2021-07-06 02:25:17 +0900

    off

--- src/utils/gtag.js
+++ src/utils/gtag.js
@@ -2,21 +2,17 @@
 export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || '';
 
 export const pageview = (path) => {
-  if (window != null) {
-    window.gtag('config', GA_ID, {
-      page_path: path,
-    });
-  }
+  window.gtag('config', GA_ID, {
+    page_path: path,
+  });
 };
 
 export const event = ({ action, category, label }) => {
   if (GA_ID === '') {
     return;
   }
-  if (window != null) {
-    window.gtag('event', action, {
-      event_category: category,
-      event_label: JSON.stringify(label),
-    });
-  }
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: JSON.stringify(label),
+  });
 };
