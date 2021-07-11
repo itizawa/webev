f70ace102b1023a214f287559de9de2673000ff9
itizawa <itizawa.pen@gmail.com>
2021-07-11 15:33:31 +0900

    fix OgpListItem

--- src/components/organisms/OgpListItem.tsx
+++ src/components/organisms/OgpListItem.tsx
@@ -111,7 +111,7 @@ export const OgpListItem: VFC<Props> = ({ page, isHideArchiveButton }) => {
                 <Icon icon={BootstrapIcon.ADD_TO_DIRECTORY} color={BootstrapColor.WHITE} />
                 <span className="ms-2">{t.move_directory}</span>
               </DropdownItem>
-              {status === PageStatus.PAGE_STATUS_ARCHIVE && (
+              {!isHideArchiveButton && status === PageStatus.PAGE_STATUS_ARCHIVE && (
                 <DropdownItem tag="button" onClick={switchArchive}>
                   <Icon height={20} width={20} icon={BootstrapIcon.REPLY} color={BootstrapColor.WHITE} />
                   <span className="ms-2 text-nowrap">{t.return_button}</span>
@@ -169,7 +169,7 @@ export const OgpListItem: VFC<Props> = ({ page, isHideArchiveButton }) => {
         </small>
         {!isHideArchiveButton && status === PageStatus.PAGE_STATUS_STOCK && (
           <StyledButton className="btn btn-sm d-flex ms-auto" onClick={switchArchive}>
-            <Icon height={20} width={20} icon={BootstrapIcon.REPLY} color={BootstrapColor.WHITE} />
+            <Icon height={20} width={20} icon={BootstrapIcon.CHECK} color={BootstrapColor.WHITE} />
             <span className="ms-2 text-nowrap">{t.read_button}</span>
           </StyledButton>
         )}
