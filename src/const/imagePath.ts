7f336258cf43ef5a573ff59412f46a3c4e46c383
itizawa <itizawa.pen@gmail.com>
2021-07-11 22:27:36 +0900

    implement 404 png

--- src/const/imagePath.ts
+++ src/const/imagePath.ts
@@ -1,5 +1,6 @@
 export const imagePath = {
   EYE_CATCH_DARK: '/images/eye-catch-dark.png',
+  NOT_FOUND_PAGE: '/images/404.png',
   NO_IMAGE: '/images/no-image.png',
   NO_PAGE: '/images/no_page.png',
 };
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,8 @@
-import { VFC } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
+import { VFC } from 'react';
+
+import { imagePath } from '~/const/imagePath';
 
 import { useLocale } from '~/hooks/useLocale';
 import { WebevOgpHead } from '~/components/Commons/WebevOgpHead';
@@ -10,8 +13,11 @@ const Page: VFC = () => {
   return (
     <>
       <WebevOgpHead title="Webev | 404" />
-      <div className="p-3">
+      <div className="p-3 text-center">
         <h1 className="m-3">{t.this_is_the_404_page}</h1>
+        <div className="w-50 mx-auto">
+          <Image src={imagePath.NOT_FOUND_PAGE} height={958} width={1000} />
+        </div>
         <h2>
           <Link href="/">
             <a className="text-white webev-anchor">{t.go_to_top}</a>
