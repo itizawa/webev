820c30298b8b5b45cf3a2d2aae9cf6253ec31be2
itizawa <itizawa.pen@gmail.com>
2021-07-13 00:35:36 +0900

    adjust style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -13,12 +13,12 @@ const Page: VFC = () => {
   return (
     <>
       <WebevOgpHead title="Webev | 404" />
-      <div className="p-3 text-center">
-        <h1 className="m-3">{t.this_is_the_404_page}</h1>
+      <div className="p-3">
+        <h1 className="m-3 text-center">{t.this_is_the_404_page}</h1>
         <div className="w-50 mx-auto">
           <Image src={imagePath.NOT_FOUND_PAGE} height={958} width={1000} />
         </div>
-        <h2>
+        <h2 className="text-center">
           <Link href="/">
             <a className="text-white webev-anchor">{t.go_to_top}</a>
           </Link>
