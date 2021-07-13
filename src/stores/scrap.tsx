ed51f46f3b4fab63d997c3c4f9d22614f0d3aaca
itizawa <itizawa.pen@gmail.com>
2021-07-14 01:06:09 +0900

    adjust store

--- src/stores/scrap.tsx
+++ src/stores/scrap.tsx
@@ -1,11 +1,10 @@
 import { SWRResponse } from 'swr';
 
 import { restClient } from '~/utils/rest-client';
-import { PaginationResult } from '~/interfaces/paginationResult';
-import { Page } from '~/domains/Page';
+import { Scrap } from '~/domains/Scrap';
 import { useAuthenticationSWR } from '~/stores/use-authentication-swr';
 
-export const useScrapById = ({ scrapId }: { scrapId: string }): SWRResponse<PaginationResult<Page>, Error> => {
+export const useScrapById = ({ scrapId }: { scrapId: string }): SWRResponse<Scrap, Error> => {
   const endpoint = scrapId != null ? `/scraps/${scrapId}` : null;
 
   return useAuthenticationSWR(endpoint, (endpoint) => restClient.apiGet(endpoint).then((result) => result.data), {
