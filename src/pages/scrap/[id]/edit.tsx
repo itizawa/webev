9a74c86e98e350524934afa05add89db3cc2e08f
itizawa <itizawa.pen@gmail.com>
2021-07-14 01:06:24 +0900

    display emoji

--- src/pages/scrap/[id]/edit.tsx
+++ src/pages/scrap/[id]/edit.tsx
@@ -1,10 +1,11 @@
-import { useState, VFC } from 'react';
+import { useState, useEffect, useRef, VFC } from 'react';
 
-import { Emoji } from 'emoji-mart';
 import Loader from 'react-loader-spinner';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
+import { Emoji, Picker, EmojiData, emojiIndex } from 'emoji-mart';
 
+import { openFileFolderEmoji } from '~/const/emoji';
 import { useLocale } from '~/hooks/useLocale';
 
 import { LoginRequiredWrapper } from '~/components/Authentication/LoginRequiredWrapper';
@@ -13,12 +14,14 @@ import { IconButton } from '~/components/Icons/IconButton';
 import { BootstrapColor, BootstrapIcon } from '~/interfaces/variables';
 import { useAllPages } from '~/stores/page';
 import { PaginationWrapper } from '~/components/Commons/PaginationWrapper';
+import { EditableInput } from '~/components/Atoms/EditableInput';
 import { NoPageAlert } from '~/components/Alerts/NoPageAlert';
 import { OgpPreviewCard } from '~/components/organisms/OgpPreviewCard';
-import { EditableInput } from '~/components/Atoms/EditableInput';
 
 import { Page } from '~/domains/Page';
 import { useScrapById } from '~/stores/scrap';
+
+const emojiSize = 40;
 
 const Index: VFC = () => {
   const { t } = useLocale();
@@ -35,6 +38,21 @@ const Index: VFC = () => {
   const [selectedPages, setSelectedPages] = useState<Page[]>([]);
 
   const { data: paginationResult } = useAllPages({ activePage, searchKeyWord });
+
+  const [isEmojiSettingMode, setIsEmojiSettingMode] = useState<boolean>();
+  const [emoji, setEmoji] = useState<EmojiData>(openFileFolderEmoji);
+  const [pickerTop, setPickerTop] = useState<number>(0);
+  const [pickerLeft, setPickerLeft] = useState<number>(0);
+  const emojiRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scrap != null) {
+      const result = emojiIndex.search(scrap.emojiId);
+      if (result != null) {
+        setEmoji(result[0]);
+      }
+    }
+  }, [scrap]);
 
   const addPageToSelectedPages = (page: Page) => {
     if (selectedPages.includes(page)) {
@@ -53,18 +71,50 @@ const Index: VFC = () => {
     setIsAddPage(false);
   };
 
+  const updateScrapTitle = (title: string) => {
+    console.log(title);
+  };
+
+  const handleSelectEmoji = async (emoji: EmojiData) => {
+    setEmoji(emoji);
+    setIsEmojiSettingMode(false);
+  };
+
+  const handleClickEmoji = () => {
+    setIsEmojiSettingMode(true);
+    if (emojiRef.current != null) {
+      setPickerTop(emojiRef.current.offsetTop + emojiSize + 10);
+      setPickerLeft(emojiRef.current.offsetLeft);
+    }
+  };
+
+  if (scrap == null) {
+    return (
+      <div className="text-center pt-5">
+        <Loader type="Triangle" color="#00BFFF" height={100} width={100} />
+      </div>
+    );
+  }
+
   return (
     <>
       <WebevOgpHead title={`Webev | New ${t.create_scrap}`} />
       <LoginRequiredWrapper>
         <div className="row pt-4">
           <div className="col-12 offset-md-2 col-md-8">
-            <h1>{t.create_scrap}</h1>
-            <div className="mb-3">
-              <label htmlFor="scrap-title" className="form-label">
-                {t.title}
-              </label>
-              <input type="text" className="form-control bg-white" id="scrap-title" placeholder={t.scrap_title_placeholder} />
+            <div className="d-flex gap-3 align-items-center my-2">
+              <div ref={emojiRef}>
+                <Emoji emoji={emoji} size={emojiSize} onClick={() => handleClickEmoji()} />
+              </div>
+              {isEmojiSettingMode && (
+                <>
+                  <div className="position-fixed top-0 start-0 end-0 bottom-0" onClick={() => setIsEmojiSettingMode(false)} />
+                  <StyledEmojiPickerWrapper top={pickerTop} left={pickerLeft}>
+                    <Picker theme="dark" onSelect={(emoji) => handleSelectEmoji(emoji)} />
+                  </StyledEmojiPickerWrapper>
+                </>
+              )}
+              <EditableInput value={scrap.title} onSubmit={updateScrapTitle} isHeader />
             </div>
             <div className="mb-3">
               <label htmlFor="scrap-body" className="form-label">
@@ -158,4 +208,11 @@ const StyledIconButtonWrapper = styled.div`
 
 const StyledDiv = styled.div`
   max-height: 300px;
+`;
+
+const StyledEmojiPickerWrapper = styled.div<{ top: number; left: number }>`
+  position: absolute;
+  top: ${(props) => props.top}px;
+  left: ${(props) => props.left}px;
+  z-index: 1300;
 `;
