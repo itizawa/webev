7f7deb0137a3bd32a265f5a602d50de2aeff2f10
itizawa <itizawa.pen@gmail.com>
2021-07-14 01:24:06 +0900

    create EditableTextares

---
+++ src/components/Atoms/EditableTextarea.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState, VFC } from 'react';
+
+import styled from 'styled-components';
+
+type Props = {
+  value: string;
+  onBlur: (inputValue: string) => void;
+  isHeader?: boolean;
+  isAllowEmpty?: boolean;
+  placeholder?: string;
+};
+
+export const EditableTextares: VFC<Props> = (props) => {
+  const { value, onBlur, isHeader, isAllowEmpty = false, placeholder } = props;
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    setInputValue(value);
+  }, [value]);
+
+  const handleKeyPress = (): void => {
+    if (!isAllowEmpty && inputValue?.trim() === '') {
+      return;
+    }
+    // do nothing, no change
+    if (inputValue === value) {
+      return;
+    }
+    onBlur(inputValue);
+  };
+
+  return (
+    <StyledInput
+      className={`form-control text-white text-nowrap overflow-scroll ${isHeader ? 'fs-1' : ''} py-0 pb-md-0 w-100`}
+      onChange={(e) => setInputValue(e.target.value)}
+      onBlur={handleKeyPress}
+      value={inputValue || ''}
+      placeholder={placeholder || ''}
+    />
+  );
+};
+
+const StyledInput = styled.textarea`
+  background: transparent;
+  border: none;
+
+  &:hover {
+    background: #232323;
+    ::placeholder {
+      color: #ccc;
+    }
+  }
+
+  &:focus {
+    background: transparent;
+    ::placeholder {
+      color: #ccc;
+    }
+  }
+`;
