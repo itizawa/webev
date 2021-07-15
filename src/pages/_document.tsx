6bb8852bcfc96f009238dda27455dd93e37e2e23
itizawa <itizawa.pen@gmail.com>
2021-07-15 21:49:06 +0900

    set getInitialProps

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 import { GA_ID } from '~/utils/gtag';
 export default class MyDocument extends Document {
   render(): JSX.Element {
@@ -39,5 +40,32 @@ export default class MyDocument extends Document {
         </body>
       </Html>
     );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  static async getInitialProps(ctx: any) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
