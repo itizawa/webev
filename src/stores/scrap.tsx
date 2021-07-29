3515d52208745ddcdb8830e7bbf482342437402a
itizawa <itizawa.pen@gmail.com>
2021-07-30 00:12:18 +0900

    implement store

--- src/stores/scrap.tsx
+++ src/stores/scrap.tsx
@@ -1,6 +1,7 @@
 import useSWR, { SWRResponse } from 'swr';
 
 import { restClient } from '~/utils/rest-client';
+import { PaginationResult } from '~/interfaces/paginationResult';
 import { Scrap } from '~/domains/Scrap';
 
 export const useScrapById = ({ scrapId }: { scrapId: string }): SWRResponse<Scrap, Error> => {
@@ -10,4 +11,18 @@ export const useScrapById = ({ scrapId }: { scrapId: string }): SWRResponse<Scra
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
   });
+};
+
+export const useScrapList = ({ activePage, searchKeyWord }: { activePage: number; searchKeyWord: string }): SWRResponse<PaginationResult<Scrap>, Error> => {
+  const limit = 30;
+
+  return useSWR(
+    ['/scraps/list', activePage, limit, searchKeyWord],
+    (endpoint, page, limit, searchKeyWord) =>
+      restClient.apiGet(`${endpoint}?page=${page}&limit=${limit}&${searchKeyWord != null ? `&q=${searchKeyWord}` : ``}`).then((result) => result.data),
+    {
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    },
+  );
 };
