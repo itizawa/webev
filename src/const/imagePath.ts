836fbbd4e72876aeec89857f629893d3be73056e
itizawa <itizawa.pen@gmail.com>
2021-07-31 16:47:14 +0900

    rename

--- src/const/imagePath.ts
+++ src/const/imagePath.ts
@@ -3,7 +3,7 @@ export const imagePath = {
   NOT_FOUND_PAGE: '/images/404.png',
   SEARCH: '/images/search.png',
   BOOKS: '/images/books.png',
-  NO_IMAGE: '/images/no-image.png',
+  NO_IMAGE: '/images/no-image-ogp.png',
   NO_PAGE: '/images/no_page.png',
   LOGO: '/images/logo.png',
   SIGN_IN_GOOGLE: '/images/sign-in-google.png',
