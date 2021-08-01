dd8989408502298bb8af0b8cb9e4a27a209318fe
itizawa <itizawa.pen@gmail.com>
2021-08-02 00:49:39 +0900

    implement Tooltip test

---
+++ src/components/base/atoms/Tooltip/Tooltip.spec.tsx
@@ -0,0 +1,20 @@
+import renderer from 'react-test-renderer';
+import { Tooltip } from './index';
+
+test('Tooltip', () => {
+  const tree = renderer
+    .create(
+      <Tooltip text="Tooltip" placement="bottom">
+        target
+      </Tooltip>,
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('Tooltip without placement', () => {
+  const tree = renderer.create(<Tooltip text="Tooltip">target</Tooltip>).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
