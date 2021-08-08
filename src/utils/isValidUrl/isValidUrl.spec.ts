7e33989ccf4df308ca8b08b737576d309a59e5f0
itizawa <itizawa.pen@gmail.com>
2021-08-08 16:09:17 +0900

    implement test

---
+++ src/utils/isValidUrl/isValidUrl.spec.ts
@@ -0,0 +1,28 @@
+import { isValidUrl } from './isValidUrl';
+
+describe('isValidUrl', (): void => {
+  test('string', (): void => {
+    const response = isValidUrl('hoge');
+    expect(response).toBeFalsy();
+  });
+
+  test('invalid', (): void => {
+    const response = isValidUrl('example.com');
+    expect(response).toBeFalsy();
+  });
+
+  test('https', (): void => {
+    const response = isValidUrl('https://example.com');
+    expect(response).toBeTruthy();
+  });
+
+  test('http', (): void => {
+    const response = isValidUrl('http://example.com');
+    expect(response).toBeTruthy();
+  });
+
+  test('with slash', (): void => {
+    const response = isValidUrl('https://example.com/');
+    expect(response).toBeTruthy();
+  });
+});
