07200d0e5436863709909539fddb5468e25a3f83
itizawa <itizawa.pen@gmail.com>
2021-08-08 22:16:10 +0900

    display loader

--- src/pages/page/[id].tsx
+++ src/pages/page/[id].tsx
@@ -1,30 +1,43 @@
 import { useRouter } from 'next/router';
-import { VFC } from 'react';
+import { ReactNode } from 'react';
+import Loader from 'react-loader-spinner';
 
 import styled from 'styled-components';
 
 import { WebevOgpHead } from '~/components/common/WebevOgpHead';
 import { LoginRequiredWrapper } from '~/components/common/Authentication/LoginRequiredWrapper';
+import { DashBoardLayout } from '~/components/common/Layout/DashBoardLayout';
 
 import { usePageByPageId } from '~/stores/page';
+import { WebevNextPage } from '~/interfaces/webevNextPage';
 
-const Index: VFC = () => {
+const Page: WebevNextPage = () => {
   const router = useRouter();
   const { id } = router.query;
 
   const { data: page } = usePageByPageId({ pageId: id as string });
 
+  if (page == null) {
+    return (
+      <div className="text-center pt-5">
+        <Loader type="Oval" color="#00BFFF" height={100} width={100} />
+      </div>
+    );
+  }
+
   return (
     <>
       <WebevOgpHead title={`Webev | ${page?.title}`} />
       <LoginRequiredWrapper>
-        <h1 className="text-center my-3">{page?.title}</h1>
-        <StyledDiv
-          className="mx-auto"
-          dangerouslySetInnerHTML={{
-            __html: `${page?.body}`,
-          }}
-        />
+        <div className="p-3">
+          <h1 className="text-center my-3">{page?.title}</h1>
+          <StyledDiv
+            className="mx-auto"
+            dangerouslySetInnerHTML={{
+              __html: `${page?.body}`,
+            }}
+          />
+        </div>
       </LoginRequiredWrapper>
     </>
   );
@@ -43,4 +56,7 @@ const StyledDiv = styled.div`
   }
 `;
 
-export default Index;
+const getLayout = (page: ReactNode) => <DashBoardLayout>{page}</DashBoardLayout>;
+
+Page.getLayout = getLayout;
+export default Page;
