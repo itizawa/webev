56c56d3a2b40c7ee018a59d73e17a5e7f50fda0a
itizawa <itizawa.pen@gmail.com>
2021-08-08 20:20:45 +0900

    adjust

--- src/utils/microCMSClient/microCMSClient.ts
+++ src/utils/microCMSClient/microCMSClient.ts
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const microCMSClient = createClient({
   serviceDomain: 'webev',
-  apiKey: process.env.CMS_API_KEY as string,
+  apiKey: (process.env.CMS_API_KEY as string) || '',
 });
