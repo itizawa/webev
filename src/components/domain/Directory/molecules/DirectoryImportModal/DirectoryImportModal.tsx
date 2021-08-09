8f8b112ad9fcbae310ff41d5a2a0c41f97405b6b
fezzlk <fezzlk796@gmail.com>
2021-08-09 20:41:55 +0900

    add file input

--- src/components/domain/Directory/molecules/DirectoryImportModal/DirectoryImportModal.tsx
+++ src/components/domain/Directory/molecules/DirectoryImportModal/DirectoryImportModal.tsx
@@ -9,10 +9,19 @@ import { useLocale } from '~/hooks/useLocale';
 export const DirectoryImportModal: VFC = () => {
   const { t } = useLocale();
 
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [isSelected, setIsSelected] = useState(false);
+
   const { data: directoryForImport, mutate: mutateDirectoryForImport } = useDirectoryForImport();
 
   const importPages = async () => {
+    console.log(await selectedFile?.text());
     console.log('import');
+  };
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedFile(e.target.files !== null ? e.target.files[0] : null);
+    setIsSelected(true);
   };
 
   const closeImportModal = async () => {
@@ -21,12 +30,14 @@ export const DirectoryImportModal: VFC = () => {
 
   return (
     <Modal isOpen={directoryForImport != null} toggle={closeImportModal} title={t.import}>
-      <h5 className="mt-3 mb-5 text-center"></h5>
+      <h5 className="mt-3 mb-5 text-center">
+        <input type="file" accept="text/html" onChange={changeHandler} />
+      </h5>
       <div className="d-flex justify-content-evenly">
         <button className="btn btn-secondary" onClick={closeImportModal}>
           {t.cancel}
         </button>
-        <button className="btn btn-primary" onClick={importPages}>
+        <button className="btn btn-primary" onClick={importPages} disabled={!isSelected}>
           {t.import}
         </button>
       </div>
