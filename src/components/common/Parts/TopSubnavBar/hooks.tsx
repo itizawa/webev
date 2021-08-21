814c687d81dfc90247a55c36ec6034d75310f192
itizawa <itizawa.pen@gmail.com>
2021-08-21 19:52:53 +0900

    implement scroll

--- src/components/common/Parts/TopSubnavBar/hooks.tsx
+++ src/components/common/Parts/TopSubnavBar/hooks.tsx
@@ -1,17 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
 import { throttle } from 'throttle-debounce';
 
+const usePrevious = (value: number): number => {
+  const ref = useRef(0);
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
 export const useHooks = (): { isShowScroll: boolean } => {
-  const [isShowScroll, setIsShowScroll] = useState(false);
+  const [currentYOffset, setCurrentYOffset] = useState(0);
+  const prevYOffset = usePrevious(currentYOffset);
+
+  const isShowScroll = useMemo(() => {
+    // 特定の高さかつ上方向にスクロールしたときに表示
+    return currentYOffset > 500 && currentYOffset < prevYOffset;
+  }, [currentYOffset, prevYOffset]);
 
   const throttleCheckScrollTop = throttle(300, () => {
     const currentYOffset = window.pageYOffset;
-    if (currentYOffset > 500) {
-      setIsShowScroll(true);
-    }
-    if (currentYOffset <= 500) {
-      setIsShowScroll(false);
-    }
+    setCurrentYOffset(currentYOffset);
   });
 
   useEffect(() => {
