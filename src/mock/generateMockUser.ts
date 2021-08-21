e110383ca0dea76d7fecc06052009ac4f3facd96
itizawa <itizawa.pen@gmail.com>
2021-08-21 23:38:32 +0900

    create generateMockUser

---
+++ src/mock/generateMockUser.ts
@@ -0,0 +1,16 @@
+import { User } from '~/domains/User';
+
+export const generateMockUser = (mock: Partial<User>): User => {
+  return new User({
+    _id: mock._id || 'mockId',
+    name: mock.name || 'mockName',
+    description: mock.description || 'mockDescription',
+    email: mock.email || 'mockEmail',
+    image: mock.image || 'mockImage',
+    admin: mock.admin || false,
+    isExecutedTutorial: mock.isExecutedTutorial || true,
+    apiTokenForExtension: mock.apiTokenForExtension || 'mockApiTokenForExtension',
+    createdAt: mock.createdAt || new Date(),
+    updatedAt: mock.updatedAt || new Date(),
+  });
+};
