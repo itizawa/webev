83df1a9b875f777789e6bc8111b1d2c5b89f96e8
itizawa <itizawa.pen@gmail.com>
2021-08-31 01:56:31 +0900

    ✨ mock next/link

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -2,11 +2,18 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-styled-components';
 import 'jsdom-global/register';
+import { ReactNode } from 'react';
 
 jest.mock('next/router', () => ({
   useRouter: () => {
     return { locale: 'ja' };
   },
 }));
+
+jest.mock('next/link', () => {
+  return ({ children }: { children: ReactNode }) => {
+    return children;
+  };
+});
 
 Enzyme.configure({ adapter: new Adapter() });
