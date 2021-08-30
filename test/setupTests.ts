0113c1344e290947fe16f4d5b0688a40406ca805
itizawa <itizawa.pen@gmail.com>
2021-08-31 01:24:52 +0900

    mock next router

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -3,4 +3,10 @@ import Adapter from 'enzyme-adapter-react-16';
 import 'jest-styled-components';
 import 'jsdom-global/register';
 
+jest.mock('next/router', () => ({
+  useRouter: () => {
+    return { locale: 'ja' };
+  },
+}));
+
 Enzyme.configure({ adapter: new Adapter() });
