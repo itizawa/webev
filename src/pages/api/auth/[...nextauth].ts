2848be33adde93048bd5cdd1cfd4d8f0c3e4947b
itizawa <itizawa.pen@gmail.com>
2022-01-29 21:59:04 +0900

    ✨ use JWT

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,21 +1,28 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-const options = {
-  site: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+
+export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
   secret: process.env.SESSION_COOKIE_SECRET,
   jwt: {
     secret: process.env.SESSION_COOKIE_SECRET,
   },
-
-  sessionMaxAge: 30 * 24 * 60 * 60 * 1000,
-  sessionUpdateAge: 24 * 60 * 60 * 1000,
-};
-
-export default (req: NextApiRequest, res: NextApiResponse): void | Promise<void> => NextAuth(req, res, options);
+});
