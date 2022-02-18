ab757d361a289bdceacadd1fa53552bc80eec700
kanoi-y <furafuratonbo@gmail.com>
2022-02-18 21:59:51 +0900

    🐛 修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { Session } from 'next-auth';
 import { Provider } from 'next-auth/client';
 import { SkeletonTheme } from 'react-loading-skeleton';
 
 import '~/styles/global.scss';
 
+import { useRouter } from 'next/router';
 import { MaintenanceLayout } from '~/components/common/Layout/MaintenanceLayout';
 
 import { usePageView } from '~/hooks/usePageView';
 import { WebevNextPage } from '~/libs/interfaces/webevNextPage';
+import { useSearchKeyWord } from '~/stores/page';
 
 const App: ({ Component, pageProps }: { Component: WebevNextPage; pageProps: { children?: ReactNode } }) => JSX.Element = ({
   Component,
@@ -18,7 +20,13 @@ const App: ({ Component, pageProps }: { Component: WebevNextPage; pageProps: { c
   pageProps: { children?: ReactNode; session?: Session };
 }) => {
   const isMaintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true';
+  const router = useRouter();
+  const { mutate: mutateSearchKeyword } = useSearchKeyWord();
   usePageView();
+
+  useEffect(() => {
+    mutateSearchKeyword('');
+  }, [router.asPath, mutateSearchKeyword]);
 
   if (isMaintenanceMode) {
     return <MaintenanceLayout />;
