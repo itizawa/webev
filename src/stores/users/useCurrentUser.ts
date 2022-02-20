0ddf86af15605a9adbb7b07be07090330ab23560
itizawa <itizawa.pen@gmail.com>
2022-02-20 23:44:30 +0900

    ✨ implement useCurrentUser

--- src/stores/users/useCurrentUser.ts
+++ src/stores/users/useCurrentUser.ts
@@ -1,12 +1,13 @@
 import useSWR, { SWRResponse } from 'swr';
 import { User } from '~/domains/User';
+import { restClient } from '~/utils/rest-client';
 
 /**
  * 現在ログインしているユーザーを返すswr
  * @returns {User} currentUser
  */
 export const useCurrentUser = (): SWRResponse<User, Error> => {
-  return useSWR('/users/me', {
+  return useSWR('/users/me', (endpoint) => restClient.apiGet(endpoint).then((result) => result.data), {
     revalidateOnFocus: false,
     revalidateOnReconnect: true,
   });
