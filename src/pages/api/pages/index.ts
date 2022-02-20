7f48765d1ec5cba717ea5f8aeb140bcdd50a5b49
itizawa <itizawa.pen@gmail.com>
2022-02-20 23:46:01 +0900

    ♻️ refactor

--- src/pages/api/pages/index.ts
+++ src/pages/api/pages/index.ts
@@ -1,26 +1,22 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiResponse } from 'next';
+import nc from 'next-connect';
 import { PostPageByUrlUseCase } from '~/application/useCases/page/PostPageByUrlUseCase';
 import { PageRepository } from '~/infrastructure/repositories/pageRepository';
-import dbConnect from '~/middlewares/dbConnect';
+import { connectDB } from '~/middlewares/dbConnect';
+
+import { WebevApiRequest } from '~/libs/interfaces/webevApiRequest';
+import { injectUserToRequest } from '~/middlewares/injectUserToRequest';
+
+const handler = nc()
+  .use(connectDB)
+  .use(injectUserToRequest)
+  .post(async (req: WebevApiRequest, res: NextApiResponse) => {
+    const { body } = req;
+
+    const page = await postPageByUrlUseCase.execute({ url: body.url, userId: '61f53b4331b03b00340cb4a0' });
+    res.status(200).json({ success: true, data: page });
+  });
+
+export default handler;
 
 const postPageByUrlUseCase = new PostPageByUrlUseCase(new PageRepository());
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { method, body } = req;
-
-  await dbConnect();
-
-  switch (method) {
-    case 'POST':
-      try {
-        const page = await postPageByUrlUseCase.execute({ url: body.url, userId: '61f53b4331b03b00340cb4a0' });
-        res.status(200).json({ success: true, data: page });
-      } catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;
-    default:
-      res.status(400).json({ success: false });
-      break;
-  }
-}
