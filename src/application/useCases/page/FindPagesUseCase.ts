fb8a87c788db7a91362fba0e59077ef1f7d3aa68
itizawa <itizawa.pen@gmail.com>
2022-02-23 22:32:56 +0900

    ✨ implement FindPagesUseCase

---
+++ src/application/useCases/page/FindPagesUseCase.ts
@@ -0,0 +1,16 @@
+import { Page } from '~/domains/Page';
+import { IPageRepository } from '~/application/repositories';
+import { PaginationOptions } from '~/libs/interfaces/pagination';
+import { PaginationResult } from '~/libs/interfaces/paginationResult';
+
+/**
+ * ページリストを取得する
+ * @returns {PaginationResult<Page>} 取得したページリスト
+ */
+export class FindPagesUseCase {
+  constructor(private readonly pageRepository: IPageRepository) {}
+
+  async execute(query: Partial<Page>, options: PaginationOptions): Promise<PaginationResult<Page>> {
+    return await this.pageRepository.find(query, options);
+  }
+}
