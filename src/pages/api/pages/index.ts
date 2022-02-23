7c940ba967c8492807338446eae3df1874832104
itizawa <itizawa.pen@gmail.com>
2022-02-23 23:43:01 +0900

    ♻️ refactor interface

--- src/pages/api/pages/index.ts
+++ src/pages/api/pages/index.ts
@@ -15,8 +15,12 @@ const handler = nc()
   .post(async (req: WebevApiRequest<{ url: string }>, res: NextApiResponse) => {
     const { body, user } = req;
 
-    const page = await postPageByUrlUseCase.execute({ url: body.url, userId: user._id });
-    res.status(200).json({ success: true, data: page });
+    try {
+      const result = await postPageByUrlUseCase.execute({ url: body.url, userId: user._id });
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ error });
+    }
   });
 
 export default handler;
