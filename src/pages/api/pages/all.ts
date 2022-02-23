4e555fe95fde24daf677c6ce01c36ca22f60bc1a
itizawa <itizawa.pen@gmail.com>
2022-02-23 23:01:54 +0900

    ✨ implement CountAllPagesUseCase

---
+++ src/pages/api/pages/all.ts
@@ -0,0 +1,19 @@
+import { NextApiResponse } from 'next';
+import nc from 'next-connect';
+import { PostPageByUrlUseCase } from '~/application/useCases/page/PostPageByUrlUseCase';
+import { PageRepository } from '~/infrastructure/repositories/pageRepository';
+import { connectDB } from '~/middlewares/dbConnect';
+
+import { WebevApiRequest } from '~/libs/interfaces/webevApiRequest';
+const postPageByUrlUseCase = new PostPageByUrlUseCase(new PageRepository());
+
+const handler = nc()
+  .use(connectDB)
+  .post(async (req: WebevApiRequest<{ url: string }>, res: NextApiResponse) => {
+    const { body, user } = req;
+
+    const page = await postPageByUrlUseCase.execute({ url: body.url, userId: user._id });
+    res.status(200).json({ success: true, data: page });
+  });
+
+export default handler;
