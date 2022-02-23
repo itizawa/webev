8d4445ccb5186d06203948cd93e7798f9ebadecd
itizawa <itizawa.pen@gmail.com>
2022-02-23 22:43:19 +0900

    ♻️ refactor search

---
+++ src/pages/api/pages/list.ts
@@ -0,0 +1,42 @@
+import { NextApiResponse } from 'next';
+import nc from 'next-connect';
+import { PageRepository } from '~/infrastructure/repositories/pageRepository';
+import { connectDB } from '~/middlewares/dbConnect';
+
+import { WebevApiRequest } from '~/libs/interfaces/webevApiRequest';
+import { injectUserToRequest } from '~/middlewares/injectUserToRequest';
+import { loginRequired } from '~/middlewares/loginRequired';
+import { FindPagesUseCase } from '~/application/useCases/page';
+import { PaginationOptions } from '~/libs/interfaces/pagination';
+import { Page } from '~/domains/Page';
+
+const findPagesUseCase = new FindPagesUseCase(new PageRepository());
+
+const handler = nc()
+  .use(connectDB)
+  .use(injectUserToRequest)
+  .use(loginRequired)
+  .get(async (req: WebevApiRequest, res: NextApiResponse) => {
+    const { user } = req;
+    const { status, directoryId, sort, page = '1', limit = '10', q = '' } = req.query;
+
+    const query: Partial<Page> & { $or?: Array<{ url?: RegExp; title?: RegExp; siteName?: RegExp; description?: RegExp }> } = { createdUser: user._id };
+    if (status) query.status = status as Page['status'];
+    if (directoryId) query.directoryId = directoryId;
+
+    if (q) {
+      query.$or = [{ url: new RegExp(q) }, { title: new RegExp(q) }, { siteName: new RegExp(q) }, { description: new RegExp(q) }];
+    }
+
+    const options = new PaginationOptions({ page: parseInt(page), limit: parseInt(limit), sort });
+
+    try {
+      const result = await findPagesUseCase.execute(query, options);
+
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  });
+
+export default handler;
