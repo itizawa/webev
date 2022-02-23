70fa49f7472b61b9387675286b2d52ddff1c54c2
itizawa <itizawa.pen@gmail.com>
2022-02-23 21:57:28 +0900

    ✨ implement useUpdateIsExecutedTutorial

--- src/hooks/Tutorial/useUpdateIsExecutedTutorial.tsx
+++ src/hooks/Tutorial/useUpdateIsExecutedTutorial.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import { User } from '~/domains/User';
 
 import { usePageListSWR } from '~/stores/page';
 import { useCurrentUser } from '~/stores/user';
-import { useSocketId } from '~/stores/contexts';
 
 import { restClient } from '~/utils/rest-client';
 import { HOW_TO_USE_URL } from '~/libs/constants/urls';
@@ -12,20 +11,20 @@ import { HOW_TO_USE_URL } from '~/libs/constants/urls';
 export const useUpdateIsExecutedTutorial = (): { isLoading: boolean; updateIsExecutedTutorial: () => Promise<void> } => {
   const { mutate: mutatePageList } = usePageListSWR();
   const { mutate: mutateCurrentUser } = useCurrentUser();
-  const { data: socketId } = useSocketId();
 
   const [isLoading, setIsLoading] = useState(false);
 
-  const updateIsExecutedTutorial = async () => {
+  const updateIsExecutedTutorial = useCallback(async () => {
     setIsLoading(true);
 
     const { data } = await restClient.apiPut<User>('/users/me/isExecutedTutorial');
-    await restClient.apiPost('/pages', { url: HOW_TO_USE_URL, socketId });
+
+    await restClient.apiPost('/pages', { url: HOW_TO_USE_URL });
     mutatePageList();
 
     setTimeout(() => mutateCurrentUser(data, false), 2000);
     setIsLoading(false);
-  };
+  }, [mutateCurrentUser, mutatePageList]);
 
   return { isLoading, updateIsExecutedTutorial };
 };
