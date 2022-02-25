7ae166e187f685342682e11f1291b88799b696ac
itizawa <itizawa.pen@gmail.com>
2022-02-26 00:10:06 +0900

    ♻️ refactor useSwitchArchive

--- src/hooks/Page/useSwitchArchive.tsx
+++ src/hooks/Page/useSwitchArchive.tsx
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Page } from '~/domains/Page';
 import { restClient } from '~/utils/rest-client';
 
-export const useSwitchArchive = (): { isLoading: boolean; switchArchive: (pageId: string, bool: boolean) => Promise<Page> } => {
+/**
+ * Archiveを切り替える
+ */
+export const useSwitchArchive = (): {
+  isLoading: boolean;
+  switchArchive: (pageId: string, bool: boolean) => Promise<Page>;
+} => {
   const [isLoading, setIsLoading] = useState(false);
 
-  const switchArchive = async (pageId: string, bool: boolean): Promise<Page> => {
+  const switchArchive = useCallback(async (pageId: string, bool: boolean): Promise<Page> => {
     setIsLoading(true);
-    const { data } = await restClient.apiPut<Page>(`/pages/${pageId}/archive`, { isArchive: bool });
-    setIsLoading(false);
+    const { data } = await restClient.apiPut<Page>(`/pages/${pageId}/archive`, { isArchive: bool }).finally(() => {
+      setIsLoading(false);
+    });
     return data;
-  };
+  }, []);
 
   return { isLoading, switchArchive };
 };
