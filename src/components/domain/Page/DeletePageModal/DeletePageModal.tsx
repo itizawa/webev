802b1236d47e4dd2634dcd59a43cbbc54160badf
itizawa <itizawa.pen@gmail.com>
2022-02-26 11:00:25 +0900

    ✨ implement DeletePageModal

--- src/components/domain/Page/DeletePageModal/DeletePageModal.tsx
+++ src/components/domain/Page/DeletePageModal/DeletePageModal.tsx
@@ -5,28 +5,28 @@ import { FixedImage } from '~/components/base/atoms/FixedImage';
 import { restClient } from '~/utils/rest-client';
 import { toastError, toastSuccess } from '~/utils/toastr';
 
-import { usePageForDelete } from '~/stores/modal';
 import { usePageListSWR } from '~/stores/page';
 
 import { useLocale } from '~/hooks/useLocale';
+import { Page } from '~/domains/Page';
 
 type Props = {
   open: boolean;
   onClose: () => void;
+  page: Page;
 };
 
-export const DeletePageModal: VFC<Props> = ({ open, onClose }) => {
+export const DeletePageModal: VFC<Props> = ({ open, onClose, page }) => {
   const { t } = useLocale();
 
-  const { data: pageForDelete, mutate: mutatePageForDelete } = usePageForDelete();
   const { mutate: pageListMutate } = usePageListSWR();
 
   const deletePage = async () => {
     try {
-      await restClient.apiDelete(`/pages/${pageForDelete?._id}`);
-      mutatePageForDelete(null);
+      await restClient.apiDelete(`/pages/${page._id}`);
       toastSuccess(t.toastr_delete_url);
       pageListMutate();
+      onClose();
     } catch (err) {
       if (err instanceof Error) toastError(err);
     }
@@ -34,8 +34,8 @@ export const DeletePageModal: VFC<Props> = ({ open, onClose }) => {
 
   return (
     <Modal isOpen={open} toggle={onClose} title={t.delete_page}>
-      <FixedImage imageUrl={pageForDelete?.image} />
-      <h5 className="card-title my-3">{pageForDelete?.title}</h5>
+      <FixedImage imageUrl={page.image} />
+      <h5 className="card-title my-3">{page.title}</h5>
       <div className="d-flex justify-content-evenly">
         <button className="btn btn-secondary" onClick={onClose}>
           {t.cancel}
