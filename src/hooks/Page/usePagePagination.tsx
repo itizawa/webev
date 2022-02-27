2a6029e8dbe62e327326594248af9f45d2dbd4c7
itizawa <itizawa.pen@gmail.com>
2022-02-27 20:08:12 +0900

    ♻️ refactor usePagePagination

--- src/hooks/Page/usePagePagination.tsx
+++ src/hooks/Page/usePagePagination.tsx
@@ -2,11 +2,12 @@ import { useContext } from 'react';
 import { PagePaginationContext } from '~/components/providers/PagePaginationProvider/PagePaginationProvider';
 
 export const usePagePagination = () => {
-  const { setSearchKeyword, setActivePage, isSortCreatedAt, setIsSortCreatedAt, pagePagination } = useContext(PagePaginationContext);
+  const { setSearchKeyword, setActivePage, isSortCreatedAt, setIsSortCreatedAt, pagePagination, mutatePagePagination } =
+    useContext(PagePaginationContext);
 
-  if (!setSearchKeyword || !setActivePage || !setIsSortCreatedAt) {
+  if (!setSearchKeyword || !setActivePage || !setIsSortCreatedAt || !mutatePagePagination) {
     throw new Error('Provider is not wrap');
   }
 
-  return { setSearchKeyword, setActivePage, isSortCreatedAt, setIsSortCreatedAt, pagePagination };
+  return { setSearchKeyword, setActivePage, isSortCreatedAt, setIsSortCreatedAt, pagePagination, mutatePagePagination };
 };
