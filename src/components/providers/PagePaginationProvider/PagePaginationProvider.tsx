696eff298fe06479e82c8e426d5a664a09ab6073
itizawa <itizawa.pen@gmail.com>
2022-02-27 20:08:01 +0900

    ♻️ refactor PagePaginationProvider

--- src/components/providers/PagePaginationProvider/PagePaginationProvider.tsx
+++ src/components/providers/PagePaginationProvider/PagePaginationProvider.tsx
@@ -1,5 +1,5 @@
 import React, { VFC, useState, createContext, ReactNode, SetStateAction, Dispatch } from 'react';
-import useSWR from 'swr';
+import useSWR, { KeyedMutator } from 'swr';
 import { Page } from '~/domains/Page';
 import { PaginationResult } from '~/libs/interfaces/paginationResult';
 import { restClient } from '~/utils/rest-client';
@@ -10,12 +10,14 @@ export const PagePaginationContext = createContext<{
   isSortCreatedAt: boolean;
   setIsSortCreatedAt?: Dispatch<SetStateAction<boolean>>;
   pagePagination?: PaginationResult<Page>;
+  mutatePagePagination?: KeyedMutator<PaginationResult<Page>>;
 }>({
   setSearchKeyword: undefined,
   setActivePage: undefined,
   isSortCreatedAt: false,
   setIsSortCreatedAt: undefined,
   pagePagination: undefined,
+  mutatePagePagination: undefined,
 });
 
 export const PagePaginationProvider: VFC<{
@@ -30,12 +32,14 @@ export const PagePaginationProvider: VFC<{
 
   const endpoint = `/pages/list?page=${activePage}&limit=${limit}&sort=${sort}${searchKeyword ? `&q=${searchKeyword}` : ``}`;
 
-  const { data: pagePagination } = useSWR<PaginationResult<Page>>(endpoint, (endpoint: string) =>
+  const { data: pagePagination, mutate: mutatePagePagination } = useSWR<PaginationResult<Page>>(endpoint, (endpoint: string) =>
     restClient.apiGet(endpoint).then((result) => result.data),
   );
 
   return (
-    <PagePaginationContext.Provider value={{ setSearchKeyword, setActivePage, isSortCreatedAt, setIsSortCreatedAt, pagePagination }}>
+    <PagePaginationContext.Provider
+      value={{ setSearchKeyword, setActivePage, isSortCreatedAt, setIsSortCreatedAt, pagePagination, mutatePagePagination }}
+    >
       {children}
     </PagePaginationContext.Provider>
   );
