50cd3a4aac2c5322f098075d4c176180c18b832b
itizawa <itizawa.pen@gmail.com>
2022-02-27 20:10:33 +0900

    ✨ implement home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,9 +3,6 @@ import { Triangle } from 'react-loader-spinner';
 
 import { WebevOgpHead } from '~/components/common/WebevOgpHead';
 
-import { usePageListSWR, useSearchKeyWord } from '~/stores/page';
-import { useLocale } from '~/hooks/useLocale';
-
 import { WebevNextPage } from '~/libs/interfaces/webevNextPage';
 
 import { SearchTextBox } from '~/components/case/molecules/SearchTextBox';
@@ -13,12 +10,12 @@ import { LoginRequiredWrapper } from '~/components/common/Authentication/LoginRe
 import { SortButtonGroup } from '~/components/common/SortButtonGroup';
 import { PageList } from '~/components/domain/Page/molecules/PageList';
 import { DashBoardLayout } from '~/components/common/Layout/DashBoardLayout';
+import { usePagePagination } from '~/hooks/Page';
+import { useLocale } from '~/hooks/useLocale';
 
 const Index: WebevNextPage = () => {
   const { t } = useLocale();
-
-  const { data: paginationResult } = usePageListSWR();
-  const { mutate: mutateSearchKeyword } = useSearchKeyWord();
+  const { pagePagination } = usePagePagination();
 
   return (
     <>
@@ -27,20 +24,20 @@ const Index: WebevNextPage = () => {
         <div className="d-flex align-items-center">
           <h1 className="mb-0">{t.home}</h1>
           <div className="ms-auto">
-            <span className="badge rounded-pill bg-secondary text-white">{paginationResult?.totalDocs} Pages</span>
+            <span className="badge rounded-pill bg-secondary text-white">{pagePagination?.totalDocs} Pages</span>
           </div>
         </div>
         <div className="my-3 d-flex flex-column flex-sm-row justify-content-between gap-3">
-          <SearchTextBox onChange={mutateSearchKeyword} />
+          <SearchTextBox />
           <SortButtonGroup />
         </div>
-        {!paginationResult && (
+        {!pagePagination && (
           <div className="pt-5 d-flex align-items-center justify-content-center">
             <Triangle color="#00BFFF" height={100} width={100} />
           </div>
         )}
-        {paginationResult && (
-          <PageList pages={paginationResult?.docs} pagingLimit={paginationResult.limit} totalItemsCount={paginationResult.totalDocs} />
+        {pagePagination && (
+          <PageList pages={pagePagination?.docs} pagingLimit={pagePagination.limit} totalItemsCount={pagePagination.totalDocs} />
         )}
       </LoginRequiredWrapper>
     </>
