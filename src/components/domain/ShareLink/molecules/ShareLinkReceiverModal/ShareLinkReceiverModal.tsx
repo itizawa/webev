a12a4a3ba54369c0f5a7d0e29bc3665e283b729c
kanoi-y <furafuratonbo@gmail.com>
2022-02-28 15:24:22 +0900

    🔨 取得中はspinnerを表示するように実装

--- src/components/domain/ShareLink/molecules/ShareLinkReceiverModal/ShareLinkReceiverModal.tsx
+++ src/components/domain/ShareLink/molecules/ShareLinkReceiverModal/ShareLinkReceiverModal.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState, VFC } from 'react';
 
+import styled from 'styled-components';
 import { Modal } from '~/components/base/molecules/Modal';
 import { usePagePagination } from '~/hooks/Page';
 import { useLocale } from '~/hooks/useLocale';
@@ -21,7 +22,7 @@ export const ShareLinkReceiverModal: VFC = () => {
   const [title, setTitle] = useState<string | null>();
   const [url, setUrl] = useState<string | null>();
 
-  const { data: ogp } = useOgp({ url: url || '' });
+  const { data: ogp, isValidating } = useOgp({ url: url || '' });
 
   useEffect(() => {
     if (typeof router.query.title === 'string') {
@@ -53,7 +54,17 @@ export const ShareLinkReceiverModal: VFC = () => {
 
   return (
     <Modal title={t.save_page} isOpen={url != null} toggle={handleClickCloseButton}>
-      <FixedImage imageUrl={ogp?.image} />
+      {isValidating ? (
+        <StyledDiv className="position-relative w-100">
+          <div className="position-absolute top-0 left-0 w-100 h-100 d-flex align-items-center justify-content-center">
+            <div className="spinner-border text-info" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        </StyledDiv>
+      ) : (
+        <FixedImage imageUrl={ogp?.image} />
+      )}
       <h5 className="text-center my-3">{title || 'No title'}</h5>
       <div className="d-flex justify-content-evenly mt-5">
         <button className="btn btn-secondary" onClick={handleClickCloseButton}>
@@ -66,3 +77,7 @@ export const ShareLinkReceiverModal: VFC = () => {
     </Modal>
   );
 };
+
+const StyledDiv = styled.div`
+  padding-top: 52.5%;
+`;
