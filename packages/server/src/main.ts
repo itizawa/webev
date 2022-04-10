d87e426ce25e6449bedff66e8659f060337dcca5
itizawa <itizawa.pen@gmail.com>
2022-04-10 20:13:11 +0900

    ✨ adjust port

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
-
-  const port = Number(process.env.PORT) || 3000; // Cloud Run の要件。環境変数PORTで起動するように。
+  const port = Number(process.env.PORT) || 8000; // Cloud Run の要件。環境変数PORTで起動するように。
   await app.listen(port, '0.0.0.0'); // '0.0.0.0' を追加して外部からのアクセスを受け入れる。
 }
 bootstrap();
