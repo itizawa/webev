50dd73708c985c83b18ebdd9823b3d586a5726de
itizawa <itizawa.pen@gmail.com>
2022-04-14 23:52:36 +0900

    🔥 delete default appModule

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { OgpModule } from './infrastructure/ioc/ogp.module';
 
 @Module({
   imports: [OgpModule],
-  providers: [AppService],
 })
 export class AppModule {}
