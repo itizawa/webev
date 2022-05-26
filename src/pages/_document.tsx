8601c05eb17d33f1c04a952cfea9c84ac42a9e37
itizawa <itizawa.pen@gmail.com>
2022-05-27 01:29:51 +0900

    ♻️ refactor document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
 import { CssBaseline } from '@nextui-org/react';
 import { GA_ID } from '~/utils/gtag';
 
-export default class MyDocument extends Document {
-  render(): JSX.Element {
+class MyDocument extends Document {
+  render() {
     return (
       <Html lang="ja">
         <Head>
@@ -47,28 +44,13 @@ export default class MyDocument extends Document {
     );
   }
 
-  static async getInitialProps(ctx: any) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      } as any;
-    } finally {
-      sheet.seal();
-    }
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: <>{initialProps.styles}</>,
+    } as unknown as DocumentInitialProps;
   }
 }
+
+export default MyDocument;
