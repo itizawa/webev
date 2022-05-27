4b7f7a006700e37c1fed9c1875217f444cabc23a
itizawa <itizawa.pen@gmail.com>
2022-05-27 21:34:33 +0900

    ♻️ refactor dahsboardLayout

--- src/components/common/Layout/DashBoardLayout.tsx
+++ src/components/common/Layout/DashBoardLayout.tsx
@@ -1,84 +1,77 @@
-import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import { VFC, ReactNode, useEffect } from 'react';
-import styled from 'styled-components';
+import { FC, ReactNode, useEffect } from 'react';
 
-import { useCurrentUser } from '~/stores/user';
+import { Container, Grid } from '@nextui-org/react';
 
 import { Navbar } from '~/components/common/Parts/Navbar';
 import { Sidebar } from '~/components/common/Sidebar';
-import { FooterSubnavBar } from '~/components/common/FooterSubnavBar';
+// import { FooterSubnavBar } from '~/components/common/FooterSubnavBar';
 import { Footer } from '~/components/common/Parts/Footer';
 
 // import { DirectoryCreateModal } from '~/components/domain/Directory/molecules/DirectoryCreateModal';
 // import { DirectoryDeleteModal } from '~/components/domain/Directory/molecules/DirectoryDeleteModal';
 // import { DirectoryRenameModal } from '~/components/domain/Directory/molecules/DirectoryRenameModal';
-import { PageSaveModal } from '~/components/domain/Page/molecules/PageSaveModal';
+// import { PageSaveModal } from '~/components/domain/Page/molecules/PageSaveModal';
 
-import { ShareLinkReceiverModal } from '~/components/domain/ShareLink/molecules/ShareLinkReceiverModal';
-import { TutorialDetectorModal } from '~/components/domain/Tutorial/molecules/TutorialDetectorModal';
+// import { ShareLinkReceiverModal } from '~/components/domain/ShareLink/molecules/ShareLinkReceiverModal';
+// import { TutorialDetectorModal } from '~/components/domain/Tutorial/molecules/TutorialDetectorModal';
 import { ScrollTopButton } from '~/components/case/atoms/ScrollTopButton';
 
 import { usePagePagination } from '~/hooks/Page';
+import { zIndex } from '~/libs/constants/zIndex';
 
 type Props = {
   children: ReactNode;
 };
 
-export const DashBoardLayout: VFC<Props> = ({ children }) => {
-  const { data: session } = useSession();
+export const DashBoardLayout: FC<Props> = ({ children }) => {
   const router = useRouter();
   const { setActivePage } = usePagePagination();
-
-  const { data: currentUser } = useCurrentUser();
 
   useEffect(() => {
     setActivePage(1);
   }, [setActivePage, router]);
 
-  if (typeof window === 'undefined') {
-    return null;
-  }
-
   return (
-    <div>
-      <div className="bg-dark">
-        <Navbar />
-      </div>
-      <StyledBorder />
-      <FooterSubnavBar />
-      <StyledDiv className="row mx-auto overflow-hidden">
-        <div className="d-none d-md-block col-md-3">
+    <Container css={{ padding: '$0', bgColor: '$background' }} fluid responsive={false}>
+      <Navbar />
+      <Grid
+        css={{
+          height: '4px',
+          width: '100%',
+          background: 'linear-gradient(90deg, #f6d02e 0, #f87c00 47%, #f6d02e);',
+          position: 'sticky',
+          top: 0,
+          zIndex: zIndex.TOP_BORDER,
+          ml: '$0',
+        }}
+      />
+      {/* <FooterSubnavBar /> */}
+      {/* 画面全体からNavbarとFooterの高さを引く */}
+      <Grid.Container css={{ minHeight: 'calc(100vh - 100px - 100px)', pt: '$8', pb: '$48', mx: 'auto' }}>
+        <Grid sm={3} css={{ display: 'none' }}>
           <Sidebar />
-        </div>
-        <div className="col-12 col-md-8 pt-3">{children}</div>
-        {session && (
-          <>
-            {/* <DirectoryCreateModal />
+        </Grid>
+        <Grid xs={12} sm={9}>
+          {children}
+        </Grid>
+      </Grid.Container>
+
+      {/* {session && (
+        <>
+          <DirectoryCreateModal />
             <DirectoryDeleteModal />
-            <DirectoryRenameModal /> */}
-            <PageSaveModal />
-          </>
-        )}
-        {session && <ShareLinkReceiverModal />}
-        {currentUser && <TutorialDetectorModal />}
-        {/* 横幅調整のためにdivでwrapしている */}
-        <div>
-          <ScrollTopButton />
-        </div>
-      </StyledDiv>
+          <DirectoryRenameModal />
+          <PageSaveModal />
+        </>
+      )}
+      {session && <ShareLinkReceiverModal />}
+      {currentUser && <TutorialDetectorModal />} */}
+      {/* 横幅調整のためにdivでwrapしている */}
+      <div>
+        <ScrollTopButton />
+      </div>
       <Footer />
-    </div>
+    </Container>
   );
 };
-
-const StyledDiv = styled.div`
-  max-width: 1440px;
-  /* 画面全体からNavbarとFooterの高さを引く */
-  min-height: calc(100vh - 100px - 100px);
-`;
-
-const StyledBorder = styled.div`
-  height: 4px;
-  background: linear-gradient(90deg, #f6d02e 0, #f87c00 47%, #f6d02e);
-`;
