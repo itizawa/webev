f9b72a0dd948a01cf6976f1ce70b2a899afb260b
itizawa <itizawa.pen@gmail.com>
2022-05-28 00:32:35 +0900

    ✨ implement PageUrlInputForm

--- src/components/common/Parts/Navbar/Navbar.tsx
+++ src/components/common/Parts/Navbar/Navbar.tsx
@@ -1,25 +1,22 @@
 import NextLink from 'next/link';
 import { FC } from 'react';
-import { Container, Grid } from '@nextui-org/react';
+import { Col, Container, Row } from '@nextui-org/react';
 
 import { PersonalDropdown } from '~/components/domain/User/PersonalDropdown';
+import { PageUrlInputForm } from '~/components/domain/Page/PageUrlInputForm';
 
 export const Navbar: FC = () => {
   return (
-    <Container fluid responsive={false} css={{ height: '56px', bgColor: '$gray100' }} display="flex" alignItems="center">
-      <Grid xs={3}>
+    <Container fluid responsive={false} css={{ height: '56px', bgColor: '$gray100', px: '$8', '@sm': { px: '$12' } }} display="flex">
+      <Row gap={0} css={{ alignItems: 'center', gap: '$8', '@sm': { gap: '$16' } }}>
         <NextLink href="/" style={{ fontWeight: 'bold', color: 'white' }}>
           Webev
         </NextLink>
-      </Grid>
-      <Grid xs={9} justify="flex-end">
-        {/* <div className="d-flex justify-content-between align-items-center">
-            <div className="col col-md-9 my-md-0 my-0 me-2">
-              <PageUrlInputForm />
-            </div>
-          </div> */}
+        <Col>
+          <PageUrlInputForm />
+        </Col>
         <PersonalDropdown />
-      </Grid>
+      </Row>
     </Container>
   );
 };
