cc602ee7f7210e8c6f0bb0bad717852fe9b86317
itizawa <itizawa.pen@gmail.com>
2022-06-03 00:55:04 +0900

    import { NoPageAlert } from '~/components/domain/Page'; ✨ implement

--- src/components/domain/Page/index.ts
+++ src/components/domain/Page/index.ts
@@ -1,4 +1,5 @@
 export { DeletePageModal } from './DeletePageModal';
+export { NoPageAlert } from './NoPageAlert';
 export { PageCountupCard } from './PageCountupCard';
 export { PageList } from './PageList';
 export { PageUrlInputForm } from './PageUrlInputForm';
