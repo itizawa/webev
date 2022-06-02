da381f032ab451aceb59dcc16ad2e448bcaa423f
itizawa <itizawa.pen@gmail.com>
2022-06-03 00:28:23 +0900

    ✨ implement archive page

--- src/pages/archived/index.tsx
+++ src/pages/archived/index.tsx
@@ -1,20 +1,24 @@
 import { ReactNode, useEffect } from 'react';
 
+import { Grid } from '@nextui-org/react';
+
 import { useLocale } from '~/hooks/useLocale';
 
-import { SearchTextBox } from '~/components/case/molecules/SearchTextBox';
 import { LoginRequiredWrapper } from '~/components/common/Authentication/LoginRequiredWrapper';
 import { SortButtonGroup } from '~/components/domain/Page/SortButtonGroup/SortButtonGroup';
 import { WebevOgpHead } from '~/components/common/WebevOgpHead';
 
+import { Loading, Text } from '~/components/uiParts';
 import { WebevNextPage } from '~/libs/interfaces/webevNextPage';
 import { DashBoardLayout } from '~/components/common/Layout/DashBoardLayout';
 import { usePagePagination } from '~/hooks/Page';
+import { PageList } from '~/components/domain/Page';
 
 const Page: WebevNextPage = () => {
   const { t } = useLocale();
 
-  const { paginationPage, setIsArchived } = usePagePagination();
+  const { paginationPage, setIsArchived, isLoadingPaginationPage } = usePagePagination();
+
   useEffect(() => {
     setIsArchived(true);
   }, [setIsArchived]);
@@ -23,26 +27,23 @@ const Page: WebevNextPage = () => {
     <>
       <WebevOgpHead title={`Webev | ${t.read}`} />
       <LoginRequiredWrapper>
-        <div className="d-flex align-items-center">
-          <h1 className="mb-0">{t.read}</h1>
-          <div className="ms-auto">
-            <span className="badge rounded-pill bg-secondary text-white">{paginationPage?.totalDocs} Pages</span>
-          </div>
-        </div>
-        <div className="my-3 d-flex flex-column flex-sm-row justify-content-between gap-3">
-          <SearchTextBox />
-          <SortButtonGroup />
-        </div>
-        {paginationPage == null && (
-          <div className="pt-5 d-flex align-items-center justify-content-center">
-            <div className="spinner-border text-info" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </div>
-          </div>
-        )}
-        {/* {paginationPage != null && (
-          <PageList pages={paginationPage.docs} pagingLimit={paginationPage.limit} totalItemsCount={paginationPage.totalDocs} />
-        )} */}
+        <Grid css={{ width: '100%', px: '$8' }}>
+          <Grid css={{ display: 'flex', alignItems: 'center' }}>
+            <Text h2>{t.read}</Text>
+            <Grid css={{ ml: 'auto', fontWeight: '$bold', color: '$white' }}>{paginationPage?.totalDocs} Pages</Grid>
+          </Grid>
+          <Grid css={{ display: 'flex', justifyContent: 'end' }}>
+            {/* <SearchTextBox /> */}
+            <SortButtonGroup />
+          </Grid>
+          {isLoadingPaginationPage ? (
+            <Grid css={{ display: 'flex', justifyContent: 'center' }}>
+              <Loading size="lg" color="secondary" />
+            </Grid>
+          ) : (
+            <>{paginationPage && <PageList pages={paginationPage?.docs} totalPages={paginationPage.totalPages} />}</>
+          )}
+        </Grid>
       </LoginRequiredWrapper>
     </>
   );
