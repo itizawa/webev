b65a03ad6c22e7881ddcf7de8cd2ac6f13d24a28
itizawa <itizawa.pen@gmail.com>
2022-06-11 18:39:39 +0900

    ♻️ refacotr

--- src/stores/Page/usePagesCountByUserId/usePagesCountByUserId.ts
+++ src/stores/Page/usePagesCountByUserId/usePagesCountByUserId.ts
@@ -6,8 +6,8 @@ import { restClient } from '~/utils/rest-client';
  * ユーザーに紐づいたページの件数を返すSWR
  * @returns {User} currentUser
  */
-export const usePagesCountByUserId = (userId: string): SWRResponse<User, Error> => {
-  return useSWR(`/users/${userId}/pages/count`, (endpoint) =>
+export const usePagesCountByUserId = (userId?: string): SWRResponse<User, Error> => {
+  return useSWR(userId ? `/users/${userId}/pages/count` : null, (endpoint) =>
     restClient.apiGet<{ currentUser: User }>(endpoint).then((result) => User.convertUserFormObject(result.data.currentUser)),
   );
 };
