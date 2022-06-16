3932bdf74719685214f65ff518a2b7f4cae65ff2
itizawa <itizawa.pen@gmail.com>
2022-06-17 00:02:06 +0900

    ♻️ refactor icon

--- src/components/base/atoms/Icon/icon.tsx
+++ src/components/base/atoms/Icon/icon.tsx
@@ -1,10 +1,9 @@
 import { FC } from 'react';
-import { BootstrapIcon } from '~/libs/interfaces/variables';
 
 type Props = {
   width?: number;
   height?: number;
-  icon: BootstrapIcon;
+  icon: keyof typeof IconComponentMapping;
 };
 
 const HomeIcon: FC = () => {
@@ -250,6 +249,12 @@ const PauseCircle: FC = () => (
   </>
 );
 
+const Sort: FC = () => (
+  <>
+    <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z" />
+  </>
+);
+
 const StopCircle: FC = () => (
   <>
     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
@@ -257,7 +262,7 @@ const StopCircle: FC = () => (
   </>
 );
 
-const IconComponentMapping = {
+export const IconComponentMapping = {
   ADD_TO_DIRECTORY: <AddToDirectoryIcon />,
   ARROW: <ArrowUpIcon />,
   ARROW_CLOCKWISE: <ArrowClockwise />,
@@ -290,6 +295,7 @@ const IconComponentMapping = {
   QUESTION: <Question />,
   REPLY: <ReplyIcon />,
   REMOVE_FROM_DIRECTORY: <RemoveFromDirectory />,
+  SORT: <Sort />,
   STAR: <StarIcon />,
   STOP_CIRCLE: <StopCircle />,
   SEARCH: <SearchIcon />,
