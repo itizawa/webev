65642a80891342d7b6fffc7a99ff765eeacd3640
itizawa <itizawa.pen@gmail.com>
2022-06-23 01:04:38 +0900

    🐛

--- src/stores/Page/usePageByPageId/usePageByPageId.ts
+++ src/stores/Page/usePageByPageId/usePageByPageId.ts
@@ -4,13 +4,7 @@ import { restClient } from '~/utils/rest-client';
 import { Page } from '~/domains/Page';
 
 export const usePageByPageId = ({ pageId }: { pageId: string }): SWRResponse<Page, Error> => {
-  return useSWR(
-    pageId ? `/pages/${pageId}` : null,
-    (endpoint: string) => restClient.apiGet<{ page: Page }>(endpoint).then((result) => result.data.page),
-    {
-      revalidateOnMount: false,
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    },
+  return useSWR(pageId ? `/pages/${pageId}` : null, (endpoint: string) =>
+    restClient.apiGet<{ page: Page }>(endpoint).then((result) => result.data.page),
   );
 };
