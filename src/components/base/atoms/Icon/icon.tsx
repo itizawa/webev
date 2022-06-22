2af86eb3a722f3b49c640f2154e7018e8beafc83
itizawa <itizawa.pen@gmail.com>
2022-06-22 23:14:39 +0900

    💄 add plus icon

--- src/components/base/atoms/Icon/icon.tsx
+++ src/components/base/atoms/Icon/icon.tsx
@@ -248,6 +248,12 @@ const PlayCircle: FC = () => (
   </>
 );
 
+const Plus: FC = () => <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />;
+
+const PlusLarge: FC = () => (
+  <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+);
+
 const PauseCircle: FC = () => (
   <>
     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
@@ -297,6 +303,8 @@ export const IconComponentMapping = {
   PENCIL: <PencilIcon />,
   PERSON: <Person />,
   PLAY_CIRCLE: <PlayCircle />,
+  PLUS: <Plus />,
+  PLUS_LARGE: <PlusLarge />,
   PLUS_BOARD: <PlusBoardIcon />,
   PLUS_DOTTED: <PlusDottedIcon />,
   QUESTION: <Question />,
