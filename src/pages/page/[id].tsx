08d36752d2b5144bac41a209a13ff09cf1a3a962
itizawa <itizawa.pen@gmail.com>
2022-06-23 00:59:17 +0900

    ✨ implement detail page

--- src/pages/page/[id].tsx
+++ src/pages/page/[id].tsx
@@ -2,7 +2,8 @@ import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 import styled from 'styled-components';
 
-import { Grid, Loading } from '@nextui-org/react';
+import { Grid, Loading, Text } from '@nextui-org/react';
+import Link from 'next/link';
 import { usePageByPageId } from '~/stores/Page';
 import { WebevNextPage } from '~/libs/interfaces/webevNextPage';
 import { useLocale } from '~/hooks/useLocale';
@@ -13,17 +14,15 @@ import { DashBoardLayout } from '~/components/common/Layout/DashBoardLayout';
 import { TopSubnavBar } from '~/components/common/Parts/TopSubnavBar';
 import { PageManageDropdown } from '~/components/domain/Page/PageManageDropdown';
 
-import { speech } from '~/utils/services';
-
 const Page: WebevNextPage = () => {
   const router = useRouter();
 
   const { id } = router.query;
 
   const { t } = useLocale();
-  const { data: page } = usePageByPageId({ pageId: id as string });
+  const { data: page, isLoading: isLoadingPage } = usePageByPageId({ pageId: id as string });
 
-  if (!page) {
+  if (isLoadingPage) {
     return (
       <Grid css={{ width: '100%', py: '$8', display: 'flex', justifyContent: 'center' }}>
         <Loading size="xl" color="secondary" />
@@ -31,37 +30,43 @@ const Page: WebevNextPage = () => {
     );
   }
 
+  if (!page) {
+    return <></>;
+  }
+
   return (
     <>
       <WebevOgpHead title={`Webev | ${page.title}`} />
       <LoginRequiredWrapper>
-        <TopSubnavBar page={page} />
-        <div className="ms-2 d-flex align-items-center">
-          <div className="ms-auto me-2">{speech.isEnabled && page.body && <></>}</div>
-          <div className="ms-2">
+        <Grid css={{ maxWidth: '800px', width: '100%' }}>
+          <TopSubnavBar page={page} />
+          <Grid css={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mb: '16px', gap: '8px' }}>
+            <Text h2 css={{ textAlign: 'center', mb: '$0', mx: 'auto', '@smMax': { fontSize: '$lg', textAlign: 'left' } }}>
+              {page.title}
+            </Text>
             <PageManageDropdown page={page} />
-          </div>
-        </div>
-        <h1 className="text-center my-3">{page.title}</h1>
-        <div className="text-center">
-          <a className="text-white webev-anchor" href={page.url} target="blank" rel="noopener noreferrer">
-            {t.view_original}
-          </a>
-        </div>
-        <StyledDiv
-          className="mx-auto mt-5"
-          dangerouslySetInnerHTML={{
-            __html: `${page?.body}`,
-          }}
-        />
+          </Grid>
+          <Text css={{ textAlign: 'center', mb: '24px' }}>
+            <Link href={page.url}>
+              <a target="_blank" rel="noopener noreferrer">
+                {t.view_original}
+              </a>
+            </Link>
+          </Text>
+          <StyledDiv
+            dangerouslySetInnerHTML={{
+              __html: `${page?.body}`,
+            }}
+          />
+        </Grid>
       </LoginRequiredWrapper>
     </>
   );
 };
 
-const StyledDiv = styled.div`
-  max-width: 800px;
+const StyledDiv = styled(Grid)`
   word-break: break-all;
+  width: 100%;
 
   img {
     width: 100%;
@@ -77,7 +82,9 @@ const StyledDiv = styled.div`
     background: black;
   }
 
-  h1 {
+  h1,
+  h2 {
+    font-size: 20px;
     padding-bottom: 5px;
     border-bottom: 2px solid #6f42c1;
   }
@@ -91,6 +98,7 @@ const StyledDiv = styled.div`
   }
 
   code {
+    background: none;
     margin: 0 5px;
   }
 `;
